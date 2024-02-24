6b020406ec3b56d94f4622d18b545206ee0ecbd5
mad360 <matt@dldipaolo.com>
2024-02-23 19:10:17 -0800

    adjust hps

--- yaat.py
+++ yaat.py
@@ -3,16 +3,16 @@ import torch, pandas, transformers
 
 # hyperparameters
 batch_size = 32
-block_size = 128
+block_size = 256
 max_iters = 5000
 eval_int = 100
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 lr = 1e-4
 eval_iters = 250
-n_embd = 600
-n_head = 10
-n_layer = 10
-dropout = 0.3
+n_embd = 300
+n_head = 6
+n_layer = 6
+dropout = 0.35
 ### ---------
 
 # load data
