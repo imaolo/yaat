e9575498117e50e6af3e2183af95b346c532d8c2
mad360 <matt@dldipaolo.com>
2024-02-27 10:00:17 -0800

    add gdown depend

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ import setuptools
 setuptools.setup(
     name='yaat',version='0.1',
     packages=setuptools.find_packages(),
-    install_requires = ['pandas', 'torch', 'transformers']
+    install_requires = ['pandas', 'torch', 'transformers', 'gdown']
 )
