74f1cf81383d3d0307a9686800b7e55be3e55082
mad360 <matt@dldipaolo.com>
2024-02-27 09:02:47 -0800

    add dependencies

--- setup.py
+++ setup.py
@@ -1,2 +1,6 @@
 import setuptools
-setuptools.setup(name='yaat',version='0.1', packages=setuptools.find_packages())
+setuptools.setup(
+    name='yaat',version='0.1',
+    packages=setuptools.find_packages(),
+    install_requires = ['pandas', 'torch', 'transformers']
+)
