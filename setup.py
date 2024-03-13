b91f1b95c3e3c9504f6f9bb772578cd602915786
mad360 <matt@dldipaolo.com>
2024-03-13 15:21:26 -0700

    remove unused, add used

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 setuptools.setup(
     name='yaat', version='0.1',
     packages=setuptools.find_packages(),
-    install_requires = [
-        'torch', 'dropbox',
-        'Informer2020 @ git+https://github.com/imaolo/Informer2020.git#egg=Informer2020']
+    install_requires = ['torch', 'python-dotenv'],
+    extras_require={'testing': ['pytest']},
+    # 'dropbox',
+    #'Informer2020 @ git+https://github.com/imaolo/Informer2020.git#egg=Informer2020']
 )
