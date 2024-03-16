4227cc9e6083f2e55675579a05e67c9ab9a62dac
mad360 <matt@dldipaolo.com>
2024-03-15 18:11:51 -0700

    add tabulate

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import setuptools
 setuptools.setup(
     name='yaat', version='0.1',
     packages=setuptools.find_packages(),
-    install_requires = ['torch', 'python-dotenv', 'Informer2020 @ git+https://github.com/imaolo/Informer2020.git#egg=Informer2020'],
+    install_requires = ['torch', 'python-dotenv', 'tabulate', 'Informer2020 @ git+https://github.com/imaolo/Informer2020.git#egg=Informer2020'],
     extras_require={'testing': ['pytest']},
 )
