31b4daefbd1c0e3e6ac8d2a120d9ad78298e3886
mad360 <matt@dldipaolo.com>
2024-03-18 18:14:04 -0700

    add gdown

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 setuptools.setup(
     name='yaat', version='0.1',
     packages=setuptools.find_packages(),
-    install_requires = ['torch', 'python-dotenv', 'tabulate', 'numpy', 'pandas',
+    install_requires = ['torch', 'python-dotenv', 'tabulate', 'numpy', 'pandas', 'gdown',
                         'Informer2020 @ git+https://github.com/imaolo/Informer2020.git#egg=Informer2020'],
     extras_require={'testing': ['pytest']},
 )
