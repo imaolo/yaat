514bb4df8f327c9f47117170a855872e8ca4b2d6
mad360 <matt@dldipaolo.com>
2024-03-24 15:15:58 -0700

    creation

---
+++ test/test_informer.py
@@ -0,0 +1,7 @@
+from yaat.informer import Informer
+import unittest
+
+class TestInformer(unittest.TestCase):
+    def setUp(self): self.informer = Informer()
+
+    def test_void(self): pass
---
+++ yaat/informer.py
@@ -0,0 +1,2 @@
+class Informer:
+    def __init__(self): pass
