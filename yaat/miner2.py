fab0ec62e472c0b6c8e3587ec343edd6ece898d6
mad360 <matt@dldipaolo.com>
2024-05-19 19:51:11 -0700

    Almost done

---
+++ yaat/miner2.py
@@ -0,0 +1,85 @@
+from yaat.util import fetchjson
+from yaat.maester2 import Maester
+from typing import List, Dict
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from pymongo.errors import DuplicateKeyError
+import time, pandas as pd
+
+# TODO - debug info
+
+class Miner:
+    alpha_url: str = 'https://www.alphavantage.co/query?'
+    alpha_key: str = 'LLE2E6Y7KG1UIS8R'
+
+    @classmethod
+    def call_alpha(cls, **kwargs) -> Dict:
+        # construct the url
+        url = cls.alpha_url + ''.join(map(lambda kv: kv[0] + '=' + str(kv[1]) + '&', kwargs.items())) + f'apikey={cls.alpha_key}'
+
+        # call it (with rate limit governance)
+        start = None
+        while start is None or (start is not None and (time.time() - start) < 62): # 75req/min
+            if 'Information' not in (data:=fetchjson(url)): return data
+            if "higher API call volume" not in data['Information']: raise RuntimeError(data)
+            if start is None: start = time.time()
+        raise RuntimeError(data)
+
+    @classmethod
+    def mine_alpha(cls, interv_min:int=15, start:datetime=datetime(2023, 2, 2, tzinfo=Maester.tz), end:datetime=datetime(2023, 4, 2, tzinfo=Maester.tz),
+                   syms:List[str]=list({'SPY', 'XLK', 'XLV', 'XLY', 'IBB', 'XLF', 'XLP', 'XLE', 'XLU', 'XLI','XLB'})):
+        assert interv_min in (1, 5, 15, 30, 60), "valid minute intervals are 1, 5, 15, 30, 60"
+
+        # parameter cleaning
+        syms = list(set(syms))
+        start = start.astimezone(Maester.tz)
+        end = end.astimezone(Maester.tz)
+
+        # find what is already in the db
+        existing_ticks = list(Maester.tickers_coll.aggregate([
+            {'$match': {
+                'datetime': {'$gte': start, '$lte': end},
+                'symbol': {'$in': syms},
+                '$expr': {'$and': [
+                    {'$eq': [{'$second': '$datetime'}, 0]},
+                    {'$in': [{'$minute': '$datetime'}, list(range(0, 60, interv_min))]}
+                ]}
+            }},
+        ]))
+
+        # get misssing symbols and datetimes
+        missing_ticks: List[Dict] = []
+        desired_ints = set(pd.date_range(start=start, end=end, freq=f"{interv_min}min"))
+        existing_ticks_df = pd.DataFrame(existing_ticks, columns=['datetime', 'symbol'])
+        for sym in syms:
+            sym_ints = set(existing_ticks_df[existing_ticks_df['symbol'] == sym]['datetime'])
+            missing_ints = desired_ints - sym_ints
+            for interv in missing_ints: missing_ticks.append({'symbol': sym, 'datetime': interv})
+
+        # get misssing symbols and datetimes by month and year
+        missing_ticks = pd.DataFrame(missing_ticks)
+        missing_ticks['year'] = missing_ticks['datetime'].dt.year
+        missing_ticks['month'] = missing_ticks['datetime'].dt.month
+        missing_ticks = missing_ticks.groupby(['symbol', 'year', 'month']).agg({'datetime': list}).reset_index()
+
+        # retrieve and insert
+        for _, mt in missing_ticks.iterrows():
+            res = cls.call_alpha(function='TIME_SERIES_INTRADAY', interval=f'{interv_min}min', symbol=mt['symbol'], month=f"{mt['year']}-{mt['month']:02}")
+            assert len(res.keys()) == 2
+            metadata = res['Meta Data']
+            assert metadata['2. Symbol'] == mt['symbol']
+            tickers: Dict = res[(set(res.keys()) - set(['Meta Data'])).pop()]
+            for time, ohlcv in tickers.items():
+                dt = datetime.strptime(time, '%Y-%m-%d %H:%M:%S').replace(tzinfo=ZoneInfo(metadata['6. Time Zone'])).astimezone(Maester.tz)
+                if dt not in mt['datetime']:
+                    try:
+                        Maester.tickers_coll.insert_one({
+                            'datetime': dt,
+                            'symbol': mt['symbol'],
+                            'open': ohlcv['1. open'],
+                            'close': ohlcv['4. close'],
+                            'high': ohlcv['2. high'],
+                            'low': ohlcv['3. low'],
+                            'volume': ohlcv['5. volume']
+                        })
+                    except DuplicateKeyError: pass # something is not right with pandas agg() but it is fine
