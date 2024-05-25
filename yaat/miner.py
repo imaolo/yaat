6f602b4a18fb8f7fae1f03ecadadfd005a8c35b0
mad360 <matt@dldipaolo.com>
2024-05-24 22:47:57 -0700

    split up a bunch of functions, refactor...

--- yaat/miner.py
+++ yaat/miner.py
@@ -1,37 +1,48 @@
 from yaat.util import fetchjson, myprint, DEBUG
-from yaat.maester2 import Maester
+from yaat.maester import Maester
 from typing import List, Dict
 from datetime import datetime
 from zoneinfo import ZoneInfo
-from pymongo.errors import DuplicateKeyError
+from dataclasses import asdict
 import time, pandas as pd
 
-# TODO - debug info
 
 class Miner:
     alpha_url: str = 'https://www.alphavantage.co/query?'
     alpha_key: str = 'LLE2E6Y7KG1UIS8R'
 
-    @classmethod
-    def call_alpha(cls, **kwargs) -> Dict:
-        # construct the url
-        url = cls.alpha_url + ''.join(map(lambda kv: kv[0] + '=' + str(kv[1]) + '&', kwargs.items())) + f'apikey={cls.alpha_key}'
-        if DEBUG: print(f"call alpha: {url}")
+    def __init__(self, maester:Maester):
+        self.maester = maester
 
-        # call it (with rate limit governance)
-        start = None
-        while start is None or (start is not None and (time.time() - start) < 62): # 75req/min
-            if 'Information' not in (data:=fetchjson(url)):
-                if DEBUG: myprint("called alpha", data)
-                return data
-            if "higher API call volume" not in data['Information']: raise RuntimeError(data)
-            if start is None: start = time.time()
-        raise RuntimeError(data)
+    def insert_ticker(self, ticker:Maester.ticker_class): self.maester.tickers_coll.insert_one(asdict(ticker))
 
-    @classmethod
-    def mine_alpha(cls, freq_min:int=15, start:datetime=datetime(2023, 2, 2, tzinfo=Maester.tz), end:datetime=datetime(2023, 4, 2, tzinfo=Maester.tz),
+    def get_existing_tickers(self, freq_min:int, start:datetime, end:datetime, syms:List[str]) -> List[Dict]:
+        self.check_freq_min(freq_min)
+        return list(self.maester.tickers_coll.aggregate([
+            {'$match': {
+                'datetime': {'$gte': start, '$lte': end},
+                'symbol': {'$in': syms},
+                '$expr': {'$and': [
+                    {'$eq': [{'$second': '$datetime'}, 0]},
+                    {'$in': [{'$minute': '$datetime'}, list(range(0, 60, freq_min))]}
+                ]}
+            }},
+        ]))
+
+    def get_missing_tickers(self, existing_ticks:List[Dict], freq_min:int, start:datetime, end:datetime, syms:List[str]) -> List[Dict[str, pd.Timestamp | List[str]]]:
+        self.check_freq_min(freq_min)
+        missing_ticks: List[Dict] = []
+        desired_ints = set(pd.date_range(start=start, end=end, freq=f"{freq_min}min"))
+        existing_ticks_df = pd.DataFrame(existing_ticks, columns=['datetime', 'symbol'])
+        for sym in syms:
+            sym_ints = set(existing_ticks_df[existing_ticks_df['symbol'] == sym]['datetime'])
+            missing_ints = desired_ints - sym_ints
+            for interv in missing_ints: missing_ticks.append({'symbol': sym, 'datetime': interv})
+        return missing_ticks
+
+    def mine_alpha(self, freq_min:int=15, start:datetime=datetime(2023, 2, 2, tzinfo=Maester.tz), end:datetime=datetime(2023, 4, 2, tzinfo=Maester.tz),
                    syms:List[str]=list({'SPY', 'XLK', 'XLV', 'XLY', 'IBB', 'XLF', 'XLP', 'XLE', 'XLU', 'XLI','XLB'})):
-        assert freq_min in (1, 5, 15, 30, 60), "valid minute intervals are 1, 5, 15, 30, 60"
+        self.check_freq_min(freq_min)
 
         # parameter cleaning
         syms = list(set(syms))
@@ -40,28 +51,11 @@ class Miner:
         if DEBUG: print(f"freq_min: {freq_min}, start: {start}, end: {end}, syms: {syms}")
 
         # find what is already in the db
-        existing_ticks = list(Maester.tickers_coll.aggregate([
-            {'$match': {
-                'datetime': {'$gte': start, '$lte': end},
-                # 'symbol': {'$in': syms},
-                # '$expr': {'$and': [
-                #     {'$eq': [{'$second': '$datetime'}, 0]},
-                #     {'$in': [{'$minute': '$datetime'}, list(range(0, 60, freq_min))]}
-                # ]}
-            }},
-        ]))
+        existing_ticks = self.get_existing_tickers(freq_min, start, end, syms)
         if DEBUG: print(f"number of existing tickers: {len(existing_ticks)}")
-        import sys
-        sys.exit()
 
         # get missing symbols and datetimes
-        missing_ticks: List[Dict] = []
-        desired_ints = set(pd.date_range(start=start, end=end, freq=f"{freq_min}min"))
-        existing_ticks_df = pd.DataFrame(existing_ticks, columns=['datetime', 'symbol'])
-        for sym in syms:
-            sym_ints = set(existing_ticks_df[existing_ticks_df['symbol'] == sym]['datetime'])
-            missing_ints = desired_ints - sym_ints
-            for interv in missing_ints: missing_ticks.append({'symbol': sym, 'datetime': interv})
+        missing_ticks = self.get_missing_tickers()
         if DEBUG: print(f"number of missing tickers: {len(missing_ticks)}")
 
         # get misssing symbols and datetimes by month and year
@@ -73,7 +67,7 @@ class Miner:
         # retrieve and insert
         for _, mt in missing_ticks.iterrows():
             if DEBUG: print("processing row"); print(mt); myprint("missing ticker datetime", mt['datetime'])
-            res = cls.call_alpha(function='TIME_SERIES_INTRADAY', outputsize='full', interval=f'{freq_min}min', symbol=mt['symbol'], month=f"{mt['year']}-{mt['month']:02}")
+            res = self.call_alpha(function='TIME_SERIES_INTRADAY', outputsize='full', interval=f'{freq_min}min', symbol=mt['symbol'], month=f"{mt['year']}-{mt['month']:02}")
             assert len(res.keys()) == 2
             metadata = res['Meta Data']
             assert metadata['2. Symbol'] == mt['symbol']
@@ -102,3 +96,22 @@ class Miner:
                         # myprint("insertion failed(3)", mt['datetime'])
                         # myprint("insertion failed(4)", [pd.Timestamp(dt)])
                         raise e
+
+    @staticmethod
+    def check_freq_min(freq_min:int): assert freq_min in (1, 5, 15, 30, 60), "valid minute intervals are 1, 5, 15, 30, 60"
+
+    @classmethod
+    def call_alpha(cls, **kwargs) -> Dict:
+        # construct the url
+        url = cls.alpha_url + ''.join(map(lambda kv: kv[0] + '=' + str(kv[1]) + '&', kwargs.items())) + f'apikey={cls.alpha_key}'
+        if DEBUG: print(f"call alpha: {url}")
+
+        # call it (with rate limit governance)
+        start = None
+        while start is None or (start is not None and (time.time() - start) < 62): # 75req/min
+            if 'Information' not in (data:=fetchjson(url)):
+                if DEBUG: myprint("called alpha", data)
+                return data
+            if "higher API call volume" not in data['Information']: raise RuntimeError(data)
+            if start is None: start = time.time()
+        raise RuntimeError(data)
