0a56cfca1355999d49f72ed590d5f72393b61a18
mad360 <matt@dldipaolo.com>
2024-05-25 21:35:20 -0700

    2005-2024 , 15m

--- scripts/mine.py
+++ scripts/mine.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     miner = Miner(Maester())
 
     syms = ['SPY', 'XLK', 'XLV', 'XLY', 'IBB', 'XLF', 'XLP', 'XLE', 'XLU', 'XLI','XLB']
-    inserted = miner.mine_alpha(15, datetime(2000, 1, 1), datetime(2024, 1, 1), syms)
+    inserted = miner.mine_alpha(15, datetime(2005, 1, 1), datetime(2024, 1, 1), syms)
     myprint("inserted", inserted)
