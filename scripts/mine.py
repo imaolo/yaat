a9639733e2066707c8a5ba2d788a6e50064fe5a5
mad360 <matt@dldipaolo.com>
2024-05-25 20:38:12 -0700

    dont start db

--- scripts/mine.py
+++ scripts/mine.py
@@ -4,7 +4,7 @@ from yaat.miner import Miner
 from datetime import datetime
 
 if __name__ == '__main__':
-    miner = Miner(Maester(connstr=None, dbdir='yaatdb_local'))
+    miner = Miner(Maester())
 
     syms = ['SPY', 'XLK', 'XLV', 'XLY', 'IBB', 'XLF', 'XLP', 'XLE', 'XLU', 'XLI','XLB']
     inserted = miner.mine_alpha(60, datetime(2010, 1, 1), datetime(2020, 1, 1), syms)
