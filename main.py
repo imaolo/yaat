35aec9d623196f0ed589bd0a07f7165af848c03d
mad360 <matt@dldipaolo.com>
2024-06-08 18:11:28 -0700

    new main with only a mine command

---
+++ main.py
@@ -0,0 +1,31 @@
+from yaat.util import DEBUG
+from yaat.maester import Maester, TimeRange, DATE_FORMAT
+import argparse
+
+### usage ### 
+
+# main parser
+main_parser = argparse.ArgumentParser(description='[YAAT] Yet Another Automated Trader')
+main_subparser = main_parser.add_subparsers(dest='cmd', required=True, help='yaat command help')
+
+# commands
+mine_parser = main_subparser.add_parser(n:='mine', help=f"{n} command help")
+
+# mine command arguments
+mine_parser.add_argument('--symbols', nargs='+', required=True, help='tickers to mine in form "sym1 sym2 sym3')
+mine_parser.add_argument('--start', type=TimeRange.clean_date, required=True, help=f"start date in the format {DATE_FORMAT}")
+mine_parser.add_argument('--end', type=TimeRange.clean_date, required=True, help=f"end date in the format {DATE_FORMAT}")
+mine_parser.add_argument('--freq_min', default=60, help=f"frequency in minutes between tickers")
+
+if __name__ == '__main__':
+
+    args = main_parser.parse_args()
+
+    if args.cmd == 'mine':
+        if DEBUG: print(f"mining {args.symbols} from {args.start} to {args.end}")
+        m = Maester()
+        for sym in args.symbols:
+            if DEBUG: print(f"mining {sym}")
+            m.alpha_mine(args.start, args.end, sym, args.freq_min)
+    else:
+        raise RuntimeError(f"improper arguemnts {args}")
