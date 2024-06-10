c46c5fb7ff11066a3c9c1abcf5718cb5770377df
mad360 <matt@dldipaolo.com>
2024-06-09 21:20:10 -0700

    add boto3 and tqdm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 setuptools.setup(
     name='yaat', version='0.1',
     packages=setuptools.find_packages(),
-    install_requires = ['python-dotenv', 'tabulate', 'pandas', 'pymongo', 'requests', 'pandas_market_calendars'],
+    install_requires = ['python-dotenv', 'tabulate', 'pandas', 'pymongo', 'requests', 'boto3', 'tqdm'],
     # 'torch'
     # 'Informer2020 @ git+https://github.com/imaolo/Informer2020.git#egg=Informer2020'
     extras_require={'testing': ['pytest']},
