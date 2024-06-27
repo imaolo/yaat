acf70326661d6231d1516d6bedaa4feba13033c2
mad360 <matt@dldipaolo.com>
2024-06-27 16:28:00 +0200

    Informer is the only depend

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ import setuptools
 setuptools.setup(
     name='yaat', version='0.1',
     packages=setuptools.find_packages(),
-    install_requires = ['python-dotenv', 'tabulate', 'pandas', 'pymongo', 'requests', 'boto3', 'tqdm'],
-    # 'torch'
-    # 'Informer2020 @ git+https://github.com/imaolo/Informer2020.git#egg=Informer2020'
+    install_requires = ['Informer2020 @ git+https://github.com/imaolo/Informer2020.git@yaat#egg=Informer2020'],
     extras_require={'testing': ['pytest']},
 )
