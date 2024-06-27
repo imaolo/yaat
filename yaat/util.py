f5208f90151e7e6bcf72052237fcdf9320757261
mad360 <matt@dldipaolo.com>
2024-06-27 16:31:29 +0200

    moved to examples

--- yaat/util.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, date
-
-DATE_FORMAT = '%Y-%m-%d'
-
-def clean_date(d: date | str) -> str:
-    if isinstance(d, date): return d.strftime(DATE_FORMAT)
-    if isinstance(d, str): return datetime.strptime(d, DATE_FORMAT).date().strftime(DATE_FORMAT)
-    raise RuntimeError(f"invalid date {d}")
