fe6407fab074bac52a0f9132a52b30deaf38afdf
mad360 <matt@dldipaolo.com>
2024-07-04 10:40:06 +0200

    itr not needed

--- yaat/informer.py
+++ yaat/informer.py
@@ -10,7 +10,7 @@ class Informer:
                  enc_in:int=6, dec_in:int=6, c_out:int=6, d_model:int=512, n_heads:int=8, e_layers:int=2, d_layers:int=1, s_layers:str='3,2,1',
                  d_ff:int=2048, factor:int=5, padding:int=0, distil:bool=True, dropout:float=0.05, attn:str='prob', embed:str='timeF',
                  activation:str='gelu', output_attention:bool=False, do_predict:bool=False, mix:bool=True, cols:Optional[str]=None, num_workers:int=0,
-                 itr:int=2, train_epochs:int=5, batch_size:int=32, patience:int=3, learning_rate:float=0.0001, des:str='test', loss:str='mse',
+                 train_epochs:int=5, batch_size:int=32, f:int=3, learning_rate:float=0.0001, des:str='test', loss:str='mse',
                  lradj:str='type1', use_amp:bool=False, inverse:bool=False, use_gpu:bool=False, gpu:int=0, use_multi_gpu:bool=False, devices:str='0,1,2,3'):
         # enc_in, dec_in, and c_out must match input feature width, no reason to parameterize here
 
