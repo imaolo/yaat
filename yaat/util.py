215068136baf6d00d875479302f21b353d5dff2d
mad360 <matt@dldipaolo.com>
2024-07-07 17:07:00 +0200

    creation - killproc() only

---
+++ yaat/util.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def killproc(proc:subprocess.Popen):
+        proc.terminate()
+        try: proc.wait(timeout=10)
+        except subprocess.TimeoutExpired:
+            print(f"process {proc.args} didn't terminate gracefully, killing it...")
+            proc.kill()
