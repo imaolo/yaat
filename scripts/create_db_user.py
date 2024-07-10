19c17f1386ef3127a28da03995a2523736b2f044
mad360 <matt@dldipaolo.com>
2024-07-10 03:48:31 -0700

    creation, not complete

---
+++ scripts/create_db_user.py
@@ -0,0 +1,22 @@
+import pymongo
+# nohup python3.11 test.py > test.log 2>&1 &
+
+# db.createUser({ user: "Earl", pwd: "fda#fDASc3.!!" , roles: [{ role: "dbOwner", db: "yaatdb"}] })
+db.createUser({
+  user: "Earl",
+  pwd: "pink-Flamingo1317",  // Choose a strong password
+  roles: [{ role: "root", db: "admin" }]
+})
+
+# get the yaat db
+coll = pymongo.MongoClient('localhost:27017')['yaatdb']['candles1min']
+
+num = coll.aggregate([
+    {'$match':{
+        'ticker': {'$in': ['SMB', 'SMBC', 'SMBK']}
+        {}
+    }},
+    {'$count': 'num'}
+])
+
+print(list(num))
