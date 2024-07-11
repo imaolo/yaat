de61cafb72c39a0fff4acfe31f01d91e609c733d
mad360 <matt@dldipaolo.com>
2024-07-11 14:32:55 -0700

    remove the graveyard

--- main.py
+++ main.py
@@ -149,19 +149,3 @@ elif args.cmd == 'maester':
             {'$limit': args.list_tickers_by_counts}
         ]))
         pprint(ticker_conts)
-
-# graveyard
-
-# exist_dataset = maester.datasets.find_one({'name':args.name})
-# if exist_dataset is not None: raise RuntimeError(f"dataset {args.name} already exists")
-
-# ohlcvs = list(maester.candles1min.aggregate([
-#     {'$match': {'ticker': {'$in': args.tickers}}}
-# ]))
-
-# pprint(ohlcvs)
-
-# maester.candles1min.database.command('collMod', maester.candles1min.name, validator={})
-# maester.candles1min.drop_indexes()
-# maester.candles1min.update_many({}, {'$rename': { 'window_start': 'date' } })
-    
