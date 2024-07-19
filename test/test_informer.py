6944b6547971522f0cf60c8b40381b9cb5aa38bd
mad360 <matt@dldipaolo.com>
2024-07-19 16:11:30 -0700

    test InformerArgs

--- test/test_informer.py
+++ test/test_informer.py
@@ -3,4 +3,12 @@ import unittest
 
 class TestInformer(unittest.TestCase):
 
-    def test_void(self): pass
+    def test_informer_args(self):
+        # test required arguments
+        with self.assertRaises(TypeError): InformerArgs()
+
+        # test succesfull with positional
+        InformerArgs('root_path_arg', 'data_path_arg')
+
+        # test succesfull with named
+        InformerArgs(root_path='root_path_arg', data_path='data_path_arg')
