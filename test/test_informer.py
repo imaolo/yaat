51e543a41516dd2ee8b0196d30139aa1ff512ccd
mad360 <matt@dldipaolo.com>
2024-07-19 16:36:35 -0700

    create informer model on test setup

--- test/test_informer.py
+++ test/test_informer.py
@@ -1,7 +1,15 @@
 from yaat.informer import Informer, InformerArgs
-import unittest
+from pathlib import Path
+import unittest, tempfile, numpy as np, pandas as pd
 
 class TestInformer(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.df = pd.DataFrame(np.random.randn(100, 3), columns=['A', 'B', 'date'])
+        cls.df_fp = Path(tempfile.NamedTemporaryFile(suffix='.csv').name)
+        cls.df.to_csv(cls.df_fp)
+        cls.informer = Informer(InformerArgs(cls.df_fp.parent, cls.df_fp.name, target='A'))
 
     def test_informer_args(self):
         # test required arguments
@@ -12,3 +20,7 @@ class TestInformer(unittest.TestCase):
 
         # test succesfull with named
         InformerArgs(root_path='root_path_arg', data_path='data_path_arg')
+
+    # def test_informer(self):
+    #     self.informer.exp_model.train(self.informer.settings)
+    #     self.informer.exp_model.predict(self.informer.settings)
