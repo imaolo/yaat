50b7713b126057d7442aefbfaac4d7064c7fb3e3
mad360 <matt@dldipaolo.com>
2024-07-19 15:39:44 -0700

    better InformerArgs formatting

--- yaat/informer.py
+++ yaat/informer.py
@@ -6,13 +6,53 @@ import torch, io, copy, time, numpy as np
 
 @dataclass
 class InformerArgs:
-    model: str = 'informer'; data: str = 'custom'; root_path: str = '..'; data_path: str = 'spy.csv'; features: str = 'MS'; target: str = 'open'
-    freq: str = 'h'; checkpoints: str = './checkpoints/'; seq_len: int = 96; label_len: int = 48; pred_len: int = 24; enc_in: int = 6; dec_in: int = 6
-    c_out: int = 6; d_model: int = 512; n_heads: int = 8; e_layers: int = 2; d_layers: int = 1; s_layers: str = '3,2,1'; d_ff: int = 2048; factor: int = 5
-    padding: int = 0; distil: bool = True; dropout: float = 0.05; attn: str = 'prob'; embed: str = 'timeF'; activation: str = 'gelu'; output_attention: bool = False
-    mix: bool = True; cols: Optional[str] = None; num_workers: int = 0; train_epochs: int = 5; batch_size: int = 32; patience: int = 3; learning_rate: float = 0.0001
-    des: str = 'test'; loss: str = 'mse'; lradj: str = 'type1'; use_amp: bool = False; inverse: bool = False; use_gpu: bool = False; gpu: int = 0
-    use_multi_gpu: bool = False; devices: str = '0,1,2,3'; std: Optional[np.array] = None; mean: Optional[np.array] = None; batch_scale:bool=False
+    model: str = 'informer'
+    data: str = 'custom'
+    root_path: str = '..'
+    data_path: str = 'spy.csv'
+    features: str = 'MS'
+    target: str = 'open'
+    freq: str = 'h'
+    checkpoints: str = './checkpoints/'
+    seq_len: int = 96
+    label_len: int = 48
+    pred_len: int = 24
+    enc_in: int = 6
+    dec_in: int = 6
+    c_out: int = 6
+    d_model: int = 512
+    n_heads: int = 8
+    e_layers: int = 2
+    d_layers: int = 1
+    s_layers: str = '3,2,1'
+    d_ff: int = 2048
+    factor: int = 5
+    padding: int = 0
+    distil: bool = True
+    dropout: float = 0.05
+    attn: str = 'prob'
+    embed: str = 'timeF'
+    activation: str = 'gelu'
+    output_attention: bool = False
+    mix: bool = True
+    cols: Optional[str] = None
+    num_workers: int = 0
+    train_epochs: int = 5
+    batch_size: int = 32
+    patience: int = 3
+    learning_rate: float = 0.0001
+    des: str = 'test'
+    loss: str = 'mse'
+    lradj: str = 'type1'
+    use_amp: bool = False
+    inverse: bool = False
+    use_gpu: bool = False
+    gpu: int = 0
+    use_multi_gpu: bool = False
+    devices: str = '0,1,2,3'
+    std: Optional[np.array] = None
+    mean: Optional[np.array] = None
+    batch_scale:bool = False
 
     @classmethod
     def from_dict(cls, args: Dict[str, Any]) -> 'InformerArgs':
