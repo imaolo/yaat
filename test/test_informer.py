4725e5767916da6e27b670250b5a5d93d2bf2d35
mad360 <matt@dldipaolo.com>
2024-07-19 17:06:42 -0700

    train() returns a generator, fix actual training issues

--- test/test_informer.py
+++ test/test_informer.py
@@ -6,11 +6,12 @@ class TestInformer(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.df = pd.DataFrame(np.random.randn(100, 3), columns=['A', 'B', 'date'])
+        cls.df = pd.DataFrame(np.random.randn(400, 3), columns=['A', 'B', 'date'])
         cls.df_fp = Path(tempfile.NamedTemporaryFile(suffix='.csv').name)
         cls.df.to_csv(cls.df_fp)
-        cls.informer = Informer(InformerArgs(cls.df_fp.parent, cls.df_fp.name, 'A'))
+        cls.informer_args = InformerArgs(cls.df_fp.parent, cls.df_fp.name, 'A', seq_len=4, pred_len=4, label_len=2, d_model=2, train_epochs=1, n_heads=1, d_ff=2)
+        cls.informer = Informer(cls.informer_args)
 
     def test_informer(self):
-        self.informer.exp_model.train(self.informer.settings)
+        list(self.informer.exp_model.train(self.informer.settings))
         self.informer.exp_model.predict(self.informer.settings)
