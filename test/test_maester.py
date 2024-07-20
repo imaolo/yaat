bd7d331cf7523a87085616172d4a032632125bb1
mad360 <matt@dldipaolo.com>
2024-07-19 20:01:13 -0700

    test document retrieve

--- test/test_maester.py
+++ test/test_maester.py
@@ -8,12 +8,19 @@ class TestMaester(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.m = Maester(connstr=os.getenv('CONNSTR'), dbdir=os.getenv('DBDIR'))
+        cls.ts = time.time()
+        cls.informer_doc = InformerDoc(root_path='somepath', data_path='somepath', target='targ', tickers=['str'],
+                                       settings='settings', name=f"{cls.ts}_name", num_params=Int64(1), fields=['field'])
+        # mongo uses millisecond precision
+        cls.informer_doc.date = cls.informer_doc.date.replace(microsecond=int(cls.informer_doc.date.microsecond / 1000) * 1000)
 
     def test_informer_doc_error(self):
         with self.assertRaises(TypeError):
             InformerDoc(root_path='somepath', data_path='somepath', target='targ')
 
     def test_informer_doc_insert(self):
-        doc = InformerDoc(root_path='somepath', data_path='somepath', target='targ', tickers=['str'],
-                           settings='settings', name=f"{time.time()}_name", num_params=Int64(1), fields=['field'])
-        self.m.informer_weights.insert_one(asdict(doc))
+        self.m.informer_weights.insert_one(asdict(self.informer_doc))
+        doc = self.m.informer_weights.find_one({'name': self.informer_doc.name}, {'_id': 0})
+        retdoc = InformerDoc(**self.m.informer_weights.find_one({'name': self.informer_doc.name}, {'_id': 0}))
+        self.assertEqual(retdoc, self.informer_doc)
+
