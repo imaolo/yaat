dd44345cc261566a39e225ae19dc6f614ec9345a
mad360 <matt@dldipaolo.com>
2024-07-19 17:02:57 -0700

    remove redundant InformerArg test

--- test/test_informer.py
+++ test/test_informer.py
@@ -9,17 +9,7 @@ class TestInformer(unittest.TestCase):
         cls.df = pd.DataFrame(np.random.randn(100, 3), columns=['A', 'B', 'date'])
         cls.df_fp = Path(tempfile.NamedTemporaryFile(suffix='.csv').name)
         cls.df.to_csv(cls.df_fp)
-        cls.informer = Informer(InformerArgs(cls.df_fp.parent, cls.df_fp.name, target='A'))
-
-    def test_informer_args(self):
-        # test required arguments
-        with self.assertRaises(TypeError): InformerArgs()
-
-        # test succesfull with positional
-        InformerArgs('root_path_arg', 'data_path_arg')
-
-        # test succesfull with named
-        InformerArgs(root_path='root_path_arg', data_path='data_path_arg')
+        cls.informer = Informer(InformerArgs(cls.df_fp.parent, cls.df_fp.name, 'A'))
 
     def test_informer(self):
         self.informer.exp_model.train(self.informer.settings)
