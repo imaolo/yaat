858a3940065d2f44e633192549eb0d6b7efec674
mad360 <matt@dldipaolo.com>
2024-07-19 19:26:55 -0700

    more decriptive function name

--- test/test_maester.py
+++ test/test_maester.py
@@ -7,6 +7,6 @@ class TestMaester(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.m = Maester(connstr=os.getenv('CONNSTR'), dbdir=os.getenv('DBDIR'))
 
-    def test_informer_doc(self):
+    def test_informer_doc_error(self):
         with self.assertRaises(TypeError):
             InformerDoc(root_path='somepath', data_path='somepath', target='targ')
