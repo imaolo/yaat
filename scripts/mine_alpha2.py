0e9312c8075231797934e82e5f5b65d9cccfed3f
mad360 <matt@dldipaolo.com>
2024-07-20 18:38:40 -0700

    make it prod

--- scripts/mine_alpha2.py
+++ scripts/mine_alpha2.py
@@ -2,16 +2,18 @@ from yaat.maester import Maester
 from datetime import datetime
 from tqdm import tqdm
 
-maester = Maester(connstr='localhost:27017')
+## BE CAREFUL this drops production data!!!
 
-tickers = ['SPY', 'AAPL']
+maester = Maester(connstr='mongodb://Earl:pink-Flamingo1317@52.91.137.11/')
+
+tickers = ['SPY', 'TSLA']
 
 print(maester.db.list_collection_names())
 with tqdm(total=len(tickers)) as pbar:
     for tick in tickers:
         pbar.set_postfix(status=tick)
         if tick in maester.db.list_collection_names(): maester.db[tick].drop()
-        maester.create_tickers_dataset(tick, start_date=datetime(2024, 6, 1))
+        maester.create_tickers_dataset(tick)
         pbar.update(1)
 print("num inserted")
 for tick in tickers:
