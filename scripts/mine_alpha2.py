81ea087371fdfc97f8dad1ae6e246ea033c63c45
mad360 <matt@dldipaolo.com>
2024-07-21 15:34:18 -0700

    use candles_db for tickers

--- scripts/mine_alpha2.py
+++ scripts/mine_alpha2.py
@@ -9,14 +9,14 @@ maester = Maester(connstr='mongodb://Earl:pink-Flamingo1317@52.91.137.11/')
 # already inserted
 # tickers = ['SPY', 'TSLA']
 
-print(maester.db.list_collection_names())
+print(maester.candles_db.list_collection_names())
 with tqdm(total=len(tickers)) as pbar:
     for tick in tickers:
         pbar.set_postfix(status=tick)
-        if tick in maester.db.list_collection_names(): maester.db[tick].drop()
+        if tick in maester.candles_db.list_collection_names(): maester.candles_db[tick].drop()
         maester.create_tickers_dataset(tick)
         pbar.update(1)
 print("num inserted")
 for tick in tickers:
-    print(tick, maester.db[tick].count_documents({}))
-print(maester.db.list_collection_names())
+    print(tick, maester.candles_db[tick].count_documents({}))
+print(maester.candles_db.list_collection_names())
