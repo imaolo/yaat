dadf340c5fa20a3594d9dec553d1338a78906f94
mad360 <matt@dldipaolo.com>
2024-07-20 18:31:55 -0700

    isolate ticker selectino to var

--- test/test_maester.py
+++ test/test_maester.py
@@ -103,10 +103,12 @@ class TestMaester(unittest.TestCase):
 
     def test_get_dataset_date_ranges(self):
 
-        start_date = self.maester.candles1min.find_one(sort=[("date", 1)])['date']
-        end_date = self.maester.candles1min.find_one(sort=[("date", -1)])['date']
+        tick='SPY'
 
-        df = self.maester.get_dataset(['SPY'], start_date=start_date, end_date=end_date)
+        start_date = self.maester.db[tick].find_one(sort=[("date", 1)])['date']
+        end_date = self.maester.db[tick].find_one(sort=[("date", -1)])['date']
+
+        df = self.maester.get_dataset([tick], start_date=start_date, end_date=end_date)
 
         self.assertEqual(df['date'].max(), end_date)
         self.assertEqual(df['date'].min(), start_date)
