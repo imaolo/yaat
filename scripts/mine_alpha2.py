886ef0a95a567a31218e52ef433862be66bf8f78
mad360 <matt@dldipaolo.com>
2024-07-21 17:35:10 -0700

    inserted ['SPY', 'QQQ', 'XLF', 'XLK', 'VXX'] w/ 1min freq

--- scripts/mine_alpha2.py
+++ scripts/mine_alpha2.py
@@ -7,16 +7,19 @@ from tqdm import tqdm
 maester = Maester(connstr='mongodb://Earl:pink-Flamingo1317@52.91.137.11/')
 
 # already inserted
-# tickers = ['SPY', 'TSLA']
+#tickers = ['SPY', 'QQQ', 'XLF', 'XLK', 'VXX']
+# freq = '1min'
 
 print(maester.candles_db.list_collection_names())
 with tqdm(total=len(tickers)) as pbar:
     for tick in tickers:
+        collname = f"{tick}_{freq}"
         pbar.set_postfix(status=tick)
-        if tick in maester.candles_db.list_collection_names(): maester.candles_db[tick].drop()
-        maester.create_tickers_dataset(tick)
+        if tick in maester.candles_db.list_collection_names(): maester.candles_db[collname].drop()
+        maester.create_tickers_dataset(tick, freq)
         pbar.update(1)
 print("num inserted")
 for tick in tickers:
-    print(tick, maester.candles_db[tick].count_documents({}))
+    collname = f"{tick}_{freq}"
+    print(collname, maester.candles_db[collname].count_documents({}))
 print(maester.candles_db.list_collection_names())
