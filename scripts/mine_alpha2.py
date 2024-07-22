ac60360770231c6ddb1d294cfe1a9ab6d75f4d79
mad360 <matt@dldipaolo.com>
2024-07-21 20:16:39 -0700

    add start_date variable

--- scripts/mine_alpha2.py
+++ scripts/mine_alpha2.py
@@ -9,6 +9,7 @@ maester = Maester(connstr='mongodb://Earl:pink-Flamingo1317@52.91.137.11/')
 # already inserted
 #tickers = ['SPY', 'QQQ', 'XLF', 'XLK', 'VXX']
 # freq = '1min'
+start_date=None
 
 print(maester.candles_db.list_collection_names())
 with tqdm(total=len(tickers)) as pbar:
@@ -16,7 +17,7 @@ with tqdm(total=len(tickers)) as pbar:
         collname = f"{tick}_{freq}"
         pbar.set_postfix(status=tick)
         if collname in maester.candles_db.list_collection_names(): maester.candles_db[collname].drop()
-        maester.create_tickers_dataset(tick, freq)
+        maester.create_tickers_dataset(tick, freq, start_date=start_date)
         pbar.update(1)
 print("num inserted")
 for tick in tickers:
