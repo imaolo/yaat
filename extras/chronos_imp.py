9e4400d36107558569ddf300975db138dc001c26
mad360 <matt@dldipaolo.com>
2024-08-24 10:13:17 -0700

    add envs and only read in certain data from frame

--- extras/chronos_imp.py
+++ extras/chronos_imp.py
@@ -1,14 +1,25 @@
 from chronos import ChronosPipeline
 from utils.tools import StandardScaler
 from pathlib import Path
-import sys, gdown, torch, numpy as np, matplotlib.pyplot as plt, pandas as pd
+import sys, os, gdown, torch, numpy as np, matplotlib.pyplot as plt, pandas as pd
 
-# create the model
+# script parameters
+MODEL = os.getenv('MODEL', 'tiny')
+NUM_DATA = os.getenv('NUM_DATA', 30)
+PRED_LEN = os.getenv('PRED_LEN', 12)
+CONTEXT_LEN = os.getenv('CONTEXT_LEN', 12)
+TEMP = os.getenv('TEMP', None)
+TOP_K = os.getenv('TOP_K', None)
+TOP_P = os.getenv('TOP_P', None)
+
+# get the device
 if torch.cuda.is_available(): device = 'cuda'
 elif torch.backends.mps.is_available(): device='mps'
 else: device = 'cpu'
+
+# create the model
 pipeline = ChronosPipeline.from_pretrained(
-    "amazon/chronos-t5-tiny",
+    "amazon/chronos-t5-"+MODEL,
     device_map=device,
     torch_dtype=torch.bfloat16,
 )
@@ -16,30 +27,25 @@ pipeline = ChronosPipeline.from_pretrained(
 # get the data
 if not (p:=Path('../spy.csv')).exists():
     gdown.download('https://drive.google.com/uc?id=16V4ZDbx_dfGL4Ky1j1FWxYaWyxG6GGra', str(p), quiet=False)
-df = pd.read_csv(str(p))
-
-# cut it down
-df = df.head(500)["open"]
-
-# define prediction parameters
-NUM_SAMPLES = 1
-PRED_LEN = 2
-CONTEXT_LEN = 48
+df = pd.read_csv(str(p), nrows=NUM_DATA, usecols=['open'])
 
 # predict!!
 forecasts = []
 for i in range(len(df)-CONTEXT_LEN-PRED_LEN-1):
     forecasts.append(pipeline.predict(
-        context=torch.tensor(df.iloc[i:i+CONTEXT_LEN].to_numpy()),
+        context=torch.tensor(df.iloc[i:i+CONTEXT_LEN].to_numpy()).squeeze(),
         prediction_length=PRED_LEN,
-        num_samples=NUM_SAMPLES).squeeze().numpy())
+        num_samples=1,
+        temperature=TEMP,
+        top_k=TOP_K,
+        top_p=TOP_P).squeeze().numpy())
 
 # calculate average loss
 loss = 0
 for idx, forecast in enumerate(forecasts):
     # get actual data
     pred_start = idx+CONTEXT_LEN+1
-    actual = df.iloc[pred_start:pred_start+PRED_LEN].to_numpy()
+    actual = df.iloc[pred_start:pred_start+PRED_LEN].to_numpy().squeeze()
 
     # scale the prediction and actual data
     scaler = StandardScaler()
