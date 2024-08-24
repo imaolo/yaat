8c7da340a7571b89b71920764632eee008143b62
mad360 <matt@dldipaolo.com>
2024-08-24 10:31:10 -0700

    print device used

--- extras/chronos_imp.py
+++ extras/chronos_imp.py
@@ -19,6 +19,7 @@ TOP_P = float(TOP_P) if TOP_P is not None else TOP_P
 if torch.cuda.is_available(): device = 'cuda'
 elif torch.backends.mps.is_available(): device='mps'
 else: device = 'cpu'
+print(f'{device=}')
 
 # create the model
 pipeline = ChronosPipeline.from_pretrained(
