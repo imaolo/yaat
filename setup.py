330e14995ad4d227267217af6a4ea588c49b8c38
mad360 <matt@dldipaolo.com>
2024-08-23 20:42:02 -0700

    add chronos

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setuptools.setup(
     name='yaat', version='0.1',
     packages=setuptools.find_packages(),
     install_requires = ['Informer2020 @ git+https://github.com/imaolo/Informer2020.git@yaat#egg=Informer2020',
+                        'git+https://github.com/amazon-science/chronos-forecasting.git',
                         'pymongo', 'pandas', 'polygon-api-client', 'requests', 'tqdm', 'gdown'],
     extras_require={'testing': ['pytest']},
 )
