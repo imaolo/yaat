ecaa80d12e08d349f092ece809257a7698a1b8bc
mad360 <matt@dldipaolo.com>
2025-02-19 14:42:24 -0800

    use common test objects

--- tests/unit/test_mongodoc.py
+++ tests/unit/test_mongodoc.py
@@ -1,24 +1,5 @@
-from yaat.mongo import MongoDoc
+from common import Doc1, Doc2, Doc1Array, Doc2Array
 import unittest
-
-class Doc1(MongoDoc):
-    f1: str
-    f2: int
-
-class Doc2(MongoDoc):
-    d1: Doc1
-    f1: int
-
-class Doc1Array(MongoDoc):
-    l1: list[str]
-    l2: list[list[int]]
-    f3: int
-
-class Doc2Array(MongoDoc):
-    l1: list[Doc1Array]
-    l2: list[list[Doc1Array]]
-    l3: list[float]
-    f1: bool
 
 class TestMongoDoc(unittest.TestCase):
     # TODO test union and optionals
