66cbe13fe319899aee0fea2ca9b912db56f978ef
mad360 <matt@dldipaolo.com>
2025-02-19 14:55:55 -0800

    remove

--- tests/integration/test_mongodoc.py
+++
@@ -1,8 +0,0 @@
-from common import Doc1, Doc2, Doc1Array, Doc2Array
-import unittest
-
-class TestMongoDoc(unittest.TestCase):
-    # TODO test union and optionals
-
-    def test_doc_success(self):
-        pass
