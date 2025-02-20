b1715aab69858c252a3eb9486212fcfec1c9469c
mad360 <matt@dldipaolo.com>
2025-02-19 22:13:45 -0800

    add mongosh download info

--- setup_server.bash
+++ setup_server.bash
@@ -11,3 +11,5 @@ git pull  https://imaolo:"$GH_PAT"@github.com/imaolo/yaat.git
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+# https://www.mongodb.com/docs/mongodb-shell/install/
