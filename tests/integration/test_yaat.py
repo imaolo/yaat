746a66f6479c528ed800af7f3a553be8316ca797
mad360 <matt@dldipaolo.com>
2025-02-19 16:46:54 -0800

    standardize some stuff

--- tests/integration/test_yaat.py
+++ tests/integration/test_yaat.py
@@ -1,10 +1,9 @@
 from pymongo import MongoClient
-from yaat.yaat import TopMoversScalper
+from yaat.yaat import TopMoversScraper
 from tests.common import docker_compose_file, IntegrationTestCase
-import unittest, pytest, time
 
 class TestYaat(IntegrationTestCase):
 
     def test_sample(self):
-        coll = MongoClient()[TopMoversScalper.dbname][TopMoversScalper.collname]
+        coll = MongoClient()[TopMoversScraper.dbname][TopMoversScraper.collname]
         self.assertGreater(coll.count_documents({}), 1)
