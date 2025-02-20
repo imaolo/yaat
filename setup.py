8c6fcff608754f2baa6e0cbc3dadf0bafb196064
mad360 <matt@dldipaolo.com>
2025-02-19 20:39:05 -0800

    add docker depen

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages # type: ignore
 
 setup(
-    name="yaat",
-    version="0.1.0",
+    name='yaat',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        "pymongo",
-        "requests",
-        "apscheduler",
-        "httpie"
+        'pymongo',
+        'requests',
+        'apscheduler',
+        'httpie'
     ],
     extras_require={
         'test': [
-            "pytest-docker",
-            "pytest"
+            'pytest-docker',
+            'pytest',
+            'docker'
         ]
     }
 )
