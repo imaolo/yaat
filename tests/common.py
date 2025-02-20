7c93acd0db742e9d9ba4934b57c2ec7905557f48
mad360 <matt@dldipaolo.com>
2025-02-19 16:46:45 -0800

    standardize some stuff

--- tests/common.py
+++ tests/common.py
@@ -1,10 +1,29 @@
 from yaat.mongo import MongoDoc
 from pathlib import Path
-import pytest, os
+from pymongo import MongoClient
+import pytest, os, unittest, time
 
 @pytest.fixture(scope="session")
 def docker_compose_file(pytestconfig):
     return os.path.join(Path(str(pytestconfig.rootdir)) / 'docker-compose.yml')
+
+class IntegrationTestCase(unittest.TestCase):
+
+    @pytest.fixture(autouse=True)
+    def inject_docker_services(self, docker_ip, docker_services):
+        self.docker_ip = docker_ip
+        self.docker_services = docker_services
+
+    def setUp(self):
+        port = self.docker_services.port_for("mongo", 27017)
+        url = "mongodb://{}:{}".format(self.docker_ip, port)
+        dbc = MongoClient(url)
+        self.docker_services.wait_until_responsive(
+            timeout=30.0,
+            pause=0.1,
+            check=lambda: dbc['admin'].command('ping')
+        )
+        time.sleep(10)
 
 class Doc1(MongoDoc):
     f1: str
