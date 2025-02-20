f02b7f438aedc87096418c10705df11d14df6e07
mad360 <matt@dldipaolo.com>
2025-02-19 21:49:18 -0800

    add web server and refactor

--- yaat/yaat.py
+++ yaat/yaat.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 from yaat.mongo import MongoCollection, MongoDoc
-from typing import Callable, final
-from apscheduler.schedulers.background import BackgroundScheduler
+from typing import final
+from apscheduler.schedulers.blocking import BlockingScheduler
 from abc import ABC, abstractmethod
-from dataclasses import asdict
 from datetime import datetime
 from wsgiref.simple_server import make_server;
-import os, requests, threading, signal, sys
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os, requests, asyncio
 
 # https://docs.coingecko.com/reference/coins-top-gainers-losers
 
@@ -34,16 +34,8 @@ COINGECKO_KEY = os.getenv('COINGECKO_KEY')
 class Scraper(ABC):
     dbname: str = 'scraper_db'
 
-    def __init__(self, mcoll:MongoCollection, interval_sec:int):
-        self.mcoll = mcoll
-        self.interval_sec = interval_sec
-
     def __call__(self, url: str, headers: dict | None = None, **kwargs) -> dict:
         return requests.get(url, headers=headers, params=kwargs).json()
-
-    @final
-    def schedule_job(self, scheduler:BackgroundScheduler):
-        scheduler.add_job(self.scrape, 'interval', seconds=self.interval_sec)
 
     @abstractmethod
     def scrape(self):
@@ -52,8 +44,8 @@ class Scraper(ABC):
 class CoinGeckoScraper(Scraper, ABC):
     api_url = "https://api.coingecko.com/api/v3/"
 
-    def __init__(self, coll:MongoCollection, interval_sec:int, api_key:str=COINGECKO_KEY):
-        super().__init__(coll, interval_sec)
+    def __init__(self, coll:MongoCollection, api_key:str=COINGECKO_KEY):
+        self.mcoll = coll
         self.headers = {"accept": "application/json", "x-cg-demo-api-key": api_key}
 
     def __call__(self, cmd:str, **kwargs) -> dict:
@@ -64,8 +56,8 @@ class TopMoversScraper(CoinGeckoScraper):
     durations: list[str] = ['1h', '24h', '7d', '14d', '30d', '1y']
     top_coins: list[str] = ['300', '500', '1000', 'all']
 
-    def __init__(self, dbc:MongoClient, interval_sec:int, api_key:str=COINGECKO_KEY):
-        super().__init__(MongoCollection(dbc[self.dbname][self.collname], TopMoverDoc), interval_sec, api_key)
+    def __init__(self, dbc:MongoClient, api_key:str=COINGECKO_KEY):
+        super().__init__(MongoCollection(dbc[self.dbname][self.collname], TopMoverDoc), api_key)
 
     def __call__(self, **kwargs) -> dict:
         return super().__call__('movers', **kwargs)
@@ -89,17 +81,25 @@ class TopMoversScraper(CoinGeckoScraper):
                         'usd_1y_change': 1,
                     })).asdict())
 
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello, world from CICD#2!")
+
 def run():
     dbc = MongoClient(host='mongo')
 
-    # create the schedule 
-    scheduler = BackgroundScheduler()
+    # create the listeners
+    hb_listener = lambda:  make_server("0.0.0.0", 8000, lambda env, res: (res('200 OK', [('Content-type', 'text/plain; charset=utf-8')]), [b"OK"])[1]).serve_forever(poll_interval=0.1)
+    web_listener = lambda:  HTTPServer(('0.0.0.0', 80), SimpleHandler).serve_forever(poll_interval=0.1)
 
-    # add to schedule
-    TopMoversScraper(dbc, 1).schedule_job(scheduler)
+    # create the schedule and add the jobs
+    scheduler = BlockingScheduler()
+    scheduler.add_job(TopMoversScraper(dbc).scrape, 'interval', seconds=1)
+    scheduler.add_job(hb_listener, 'date', run_date=datetime.now())
+    scheduler.add_job(web_listener, 'date', run_date=datetime.now())
 
     # start the schedule
     scheduler.start()
-
-    # start the heardbeat
-    make_server("0.0.0.0", 8000, lambda env, res: (res('200 OK', [('Content-type', 'text/plain; charset=utf-8')]), [b"OK"])[1]).serve_forever()
