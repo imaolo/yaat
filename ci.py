93768427a8fd942ee4fa79fb3c5e5f51bd662521
mad360 <matt@dldipaolo.com>
2025-02-19 22:38:09 -0800

    add DEPLOY env

--- ci.py
+++ ci.py
@@ -4,9 +4,10 @@ SSH_USERNAME = os.getenv('SSH_USERNAME')
 SSH_HOST = os.getenv('SSH_HOST')
 SSH_KEY = os.getenv('SSH_KEY')
 GH_PAT = os.getenv('GH_PAT')
+DEPLOY = os.getenv('DEPLOY')
 
-if not SSH_USERNAME or not SSH_KEY or not SSH_HOST or not GH_PAT:
-    raise RuntimeError("SSH_USERNAME, SSH_KEY, SSH_HOST, and GH_PAT required")
+if DEPLOY and (not GH_PAT or not SSH_KEY or not SSH_HOST or not SSH_USERNAME):
+    raise RuntimeError("Deploy requires GH_PAT, SSH_KEY, SSH_HOST, and SSH_USERNAME")
 
 def runcmd(cmd:str) -> None | str:
     print(cmd)
@@ -14,9 +15,10 @@ def runcmd(cmd:str) -> None | str:
 
 if __name__ == "__main__":
     runcmd('python -m pytest tests')
-    ssh_cmd = "cd yaat && "
-    ssh_cmd += f"git pull https://imaolo:{GH_PAT}@github.com/imaolo/yaat && "
-    ssh_cmd += "docker-compose up --build --force-recreate --remove-orphans -d"
-    runcmd('echo "$SSH_KEY" > ~/id_rsa.pem')
-    os.chmod(os.path.expanduser('~/id_rsa.pem'), 0o600)
-    runcmd(f'ssh -o StrictHostKeyChecking=no -i ~/id_rsa.pem {SSH_USERNAME}@{SSH_HOST} "{ssh_cmd}"')
+    if DEPLOY:
+        ssh_cmd = "cd yaat && "
+        ssh_cmd += f"git pull https://imaolo:{GH_PAT}@github.com/imaolo/yaat && "
+        ssh_cmd += "docker-compose up --build --force-recreate --remove-orphans -d"
+        runcmd('echo "$SSH_KEY" > ~/id_rsa.pem')
+        os.chmod(os.path.expanduser('~/id_rsa.pem'), 0o600)
+        runcmd(f'ssh -o StrictHostKeyChecking=no -i ~/id_rsa.pem {SSH_USERNAME}@{SSH_HOST} "{ssh_cmd}"')
