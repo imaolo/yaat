81f02980eeceac813f38d6dcf8af629868529616
mad360 <matt@dldipaolo.com>
2025-02-19 20:36:53 -0800

    GITHUB -> GH

--- ci.py
+++ ci.py
@@ -3,10 +3,10 @@ import os, subprocess
 SSH_USERNAME = os.getenv('SSH_USERNAME')
 SSH_HOST = os.getenv('SSH_HOST')
 SSH_KEY = os.getenv('SSH_KEY')
-GITHUB_PAT = os.getenv('GITHUB_PAT')
+GH_PAT = os.getenv('GH_PAT')
 
-if not SSH_USERNAME or not SSH_KEY or not SSH_HOST or not GITHUB_PAT:
-    raise RuntimeError("SSH_USERNAME, SSH_KEY, SSH_HOST, and GITHUB_PAT required")
+if not SSH_USERNAME or not SSH_KEY or not SSH_HOST or not GH_PAT:
+    raise RuntimeError("SSH_USERNAME, SSH_KEY, SSH_HOST, and GH_PAT required")
 
 def runcmd(cmd:str) -> None | str:
     print(cmd)
@@ -14,8 +14,8 @@ def runcmd(cmd:str) -> None | str:
 
 if __name__ == "__main__":
     runcmd('python -m pytest tests')
-    ssh_cmd = "cd test-cicd && "
-    ssh_cmd += f"git pull https://imaolo:{GITHUB_PAT}@github.com/imaolo/test-cicd && "
+    ssh_cmd = "cd yaat && "
+    ssh_cmd += f"git pull https://imaolo:{GH_PAT}@github.com/imaolo/yaat && "
     ssh_cmd += "docker-compose up --build --force-recreate --remove-orphans -d"
     runcmd('echo "$SSH_KEY" > ~/id_rsa.pem')
     os.chmod(os.path.expanduser('~/id_rsa.pem'), 0o600)
--- setup_server.bash
+++ setup_server.bash
@@ -5,8 +5,8 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 docker ps
 
-git clone https://imaolo:<PAT>@github.com/imaolo/test-cicd.git
-git pull  https://imaolo:<PAT>@github.com/imaolo/test-cicd.git
+git clone https://imaolo:"$GH_PAT"@github.com/imaolo/yaat.git
+git pull  https://imaolo:"$GH_PAT"@github.com/imaolo/yaat.git
 
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
