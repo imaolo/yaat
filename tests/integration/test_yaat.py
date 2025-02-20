5b5efed0ca480805fd064d9659a8b80594c31557
mad360 <matt@dldipaolo.com>
2025-02-19 16:35:08 -0800

    use common test objects now

--- tests/integration/test_yaat.py
+++ tests/integration/test_yaat.py
@@ -1,25 +1,9 @@
 from pymongo import MongoClient
 from yaat.yaat import TopMoversScalper
-from tests.common import docker_compose_file
+from tests.common import docker_compose_file, IntegrationTestCase
 import unittest, pytest, time
 
-class TestYaat(unittest.TestCase):
-
-    @pytest.fixture(autouse=True)
-    def inject_docker_services(self, docker_ip, docker_services):
-        self.docker_ip = docker_ip
-        self.docker_services = docker_services
-
-    def setUp(self):
-        port = self.docker_services.port_for("mongo", 27017)
-        url = "mongodb://{}:{}".format(self.docker_ip, port)
-        dbc = MongoClient(url)
-        self.docker_services.wait_until_responsive(
-            timeout=30.0,
-            pause=0.1,
-            check=lambda: dbc['admin'].command('ping')
-        )
-        time.sleep(10)
+class TestYaat(IntegrationTestCase):
 
     def test_sample(self):
         coll = MongoClient()[TopMoversScalper.dbname][TopMoversScalper.collname]
