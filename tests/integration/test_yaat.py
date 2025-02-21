5c1e07c041be98428f5b608c626507b68ce379af
mad360 <matt@dldipaolo.com>
2025-02-21 15:35:06 -0800

    use new scraper api

--- tests/integration/test_yaat.py
+++ tests/integration/test_yaat.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
-from yaat.yaat import TopMoversScraper
+from yaat.yaat import Scraper, TopMoverDoc
 from tests.common import IntegrationTestCase
 
 class TestYaat(IntegrationTestCase):
 
     def test_TopMoversScraper(self):
-        coll = TopMoversScraper(MongoClient()).coll
+        coll = Scraper(MongoClient(), TopMoverDoc).coll
         self.docker_services.wait_until_responsive(
             timeout=10.0,
             pause=0.1,
