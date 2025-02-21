4bf07f4b0f69aa706d8c1bf9434274d83db1bf87
mad360 <matt@dldipaolo.com>
2025-02-21 15:50:18 -0800

    refactor

--- tests/integration/test_yaat.py
+++ tests/integration/test_yaat.py
@@ -5,9 +5,9 @@ from tests.common import IntegrationTestCase
 class TestYaat(IntegrationTestCase):
 
     def test_TopMoversScraper(self):
-        coll = Scraper(MongoClient(), TopMoverDoc).coll
+        scraper = Scraper(MongoClient(), TopMoverDoc)
         self.docker_services.wait_until_responsive(
             timeout=10.0,
             pause=0.1,
-            check=lambda: (coll.count_documents({}) > 0)
+            check=lambda: (scraper.coll.count_documents({}) > 0)
         )
