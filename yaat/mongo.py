bfd2f643750607714791eb6f467c2be2efa05f74
mad360 <matt@dldipaolo.com>
2025-02-22 16:21:46 -0800

    no more drop index

--- yaat/mongo.py
+++ yaat/mongo.py
@@ -93,7 +93,6 @@ class MongoDoc(abc.ABC):
         else:
             db.create_collection(cls.collname, validator=cls.schema)
         coll = db[cls.collname]
-        coll.drop_indexes() # TODO - remove
 
         # create index
         if cls.colldoc:
