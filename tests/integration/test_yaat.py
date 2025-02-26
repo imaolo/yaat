87604507bc554ffe59aaad8ff2a35c3be8ad615f
mad360 <matt@dldipaolo.com>
2025-02-26 10:31:48 -0800

    test the prices scraper

--- tests/integration/test_yaat.py
+++ tests/integration/test_yaat.py
@@ -6,3 +6,7 @@ class TestYaat(IntegrationTestCase):
     def test_TopMoversScraper(self):
         coll = YaatDBInstance().scraper_db.top_movers
         wait_until_true(lambda: (coll.count_documents({}) > 0), 10, 1)
+
+    def test_PricesScraper(self):
+        coll = YaatDBInstance().scraper_db.prices
+        wait_until_true(lambda: (coll.count_documents({}) > 0), 10, 1)
