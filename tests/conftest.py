417c9854469ac74026a40209e5d820af7bc75d97
mad360 <matt@dldipaolo.com>
2025-02-25 22:55:16 -0800

    create a class only fixture for IntegrationTestCase

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,9 @@ import pytest, os, pathlib
 @pytest.fixture(scope="session")
 def docker_compose_file(pytestconfig):
     return os.path.join(pathlib.Path(str(pytestconfig.rootdir)) / 'docker-compose.yml')
+
+@pytest.fixture(scope="class")
+def init_class_docker(request, docker_services, docker_ip):
+    request.cls.docker_ip = docker_ip
+    request.cls.listen_services()
+    yield
