ad27d96e3133ccb2d7a7d0148f280b6833c7c967
mad360 <matt@dldipaolo.com>
2025-02-26 17:51:10 -0800

    load_dotenv

--- yaat/helpers.py
+++ yaat/helpers.py
@@ -1,5 +1,8 @@
 from typing import Any, Callable
+from dotenv import load_dotenv
 import os, time
+
+load_dotenv()
 
 def getenv(key:str, default:Any=0): return type(default)(int(os.getenv(key, default)) if isinstance(default, bool) else os.getenv(key, default))
 def wait_until_true(fn: Callable[[], bool], timeout:float=30.0, pause:float=5.0, msg:str=''):
