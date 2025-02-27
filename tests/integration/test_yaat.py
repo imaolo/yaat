782152d5f925bcf2404404473260f91d103e7b11
mad360 <matt@dldipaolo.com>
2025-02-27 09:25:48 -0800

    manual scrape

--- tests/integration/test_yaat.py
+++ tests/integration/test_yaat.py
@@ -1,12 +1,21 @@
-from yaat.yaat import YaatDBInstance
+from yaat.yaat import YaatDBInstance, ScraperCollection
+from yaat.mongo import MongoCollection
 from yaat.helpers import wait_until_true
 from tests.common import IntegrationTestCase
 
 class TestYaat(IntegrationTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.dbi = YaatDBInstance()
+
+    def helper_test_scraper_coll(self, coll: ScraperCollection):
+        prev_docs = coll.count_documents({})
+        coll.scrape()
+        self.assertGreater(coll.count_documents({}), prev_docs)
+
     def test_TopMoversScraper(self):
-        coll = YaatDBInstance().scraper_db.top_movers
-        wait_until_true(lambda: (coll.count_documents({}) > 0), 10, 1)
+        self.helper_test_scraper_coll(self.dbi.scraper_db.top_movers)
 
     def test_PricesScraper(self):
-        coll = YaatDBInstance().scraper_db.prices
-        wait_until_true(lambda: (coll.count_documents({}) > 0), 10, 1)
+        self.helper_test_scraper_coll(self.dbi.scraper_db.prices)
