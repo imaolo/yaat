406e1c7895c061b3c5acdca5aec8d094d6ab428c
mad360 <matt@dldipaolo.com>
2025-02-28 15:47:28 -0800

    run scraper outside of blocing schedule

--- yaat/yaat.py
+++ yaat/yaat.py
@@ -25,8 +25,9 @@ def run():
     connect(db='yaatdb', host='mongo' if DOCKER else None, timeoutMS=3000)
     _make_listener = lambda handler, port: lambda: make_server(ip, port, handler).serve_forever(poll_interval=0.1)
 
+    run_scraper()
+
     scheduler = BlockingScheduler()
-    scheduler.add_job(run_scraper, 'date', run_date=datetime.now())
     scheduler.add_job(_make_listener(hb_handler, 8000), 'date', run_date=datetime.now())
     scheduler.add_job(_make_listener(status_handler, 80), 'date', run_date=datetime.now())
     scheduler.start()
