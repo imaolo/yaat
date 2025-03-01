1e17fbd6824d7eeeb429b254f4c34a2553ee6cc1
mad360 <matt@dldipaolo.com>
2025-02-28 19:06:36 -0800

    kill on exit listener

--- yaat/yaat.py
+++ yaat/yaat.py
@@ -1,33 +1,26 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.events import EVENT_JOB_ERROR
 from mongoengine import connect
 from datetime import datetime
 from yaat.scraper import run as run_scraper
+from yaat.webapp import run as run_webapp
 from yaat.helpers import getenv
-from yaat.webapp import WEBAPP
-import threading, time, sys
+import os, signal
 
 def run():
     # create the schedule
-    scheduler = BackgroundScheduler()
+    scheduler = BlockingScheduler()
 
     # add the jobs    
-    scheduler.add_job(WEBAPP.run, 'date', run_date=datetime.now())
+    scheduler.add_job(run_webapp, 'date', run_date=datetime.now())
     scheduler.add_job(run_scraper, 'date', run_date=datetime.now())
 
     # exit listener
-    shutdown_event = threading.Event()
     def exit(event):
         if event.exception:
-            shutdown_event.set()
-            WEBAPP.should_exit = True
-            scheduler.shutdown()
+            os.kill(os.getpid(), signal.SIGTERM)
     scheduler.add_listener(exit, EVENT_JOB_ERROR)
 
-    # connect to the database and start the app
+    # connect to the database and start the schedule
     connect(db='yaatdb', host='mongo' if getenv('DOCKER', False) else None, timeoutMS=3000)
     scheduler.start()
-
-    # wait for exit
-    while not shutdown_event.is_set(): time.sleep(0.1)
-    sys.exit(1)
