08c24a503ffe1434a80fe8015d99b00e5bd8427f
mad360 <matt@dldipaolo.com>
2025-02-28 17:17:51 -0800

    use FastAPI

--- yaat/yaat.py
+++ yaat/yaat.py
@@ -2,34 +2,28 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from mongoengine import connect
 from datetime import datetime
 from wsgiref.simple_server import make_server
-from yaat.scraper import run as run_scraper, TopMoverDoc
 from yaat.helpers import getenv
+from yaat.webapp import run as run_webapp
 
 DOCKER = getenv('DOCKER', False)
 
-ip = '0.0.0.0'
 header = [('Content-type', 'text/plain; charset=utf-8')]
-status = '200 OK'
-
-def status_handler(_, res):
-    body = f"number of top mover docs {TopMoverDoc.objects.count()}".encode()
-    headers = header + [('Content-Length', str(len(body)))]
-    res(status, headers)
-    return [body]
-
 def hb_handler(_, res):
     body = b"OK"
     headers = header + [('Content-Length', str(len(body)))]
-    res(status, headers)
+    res('200 OK', headers)
     return [body]
+
+def run_hb():
+    make_server('0.0.0.0', 8000, hb_handler).serve_forever(poll_interval=0.1)
 
 def run():
     connect(db='yaatdb', host='mongo' if DOCKER else None, timeoutMS=3000)
-    _make_listener = lambda handler, port: lambda: make_server(ip, port, handler).serve_forever(poll_interval=0.1)
-
-    run_scraper()
 
     scheduler = BlockingScheduler()
-    scheduler.add_job(_make_listener(hb_handler, 8000), 'date', run_date=datetime.now())
-    scheduler.add_job(_make_listener(status_handler, 80), 'date', run_date=datetime.now())
+
+    scheduler.add_job(run_hb, 'date', run_date=datetime.now())
+    scheduler.add_job(run_webapp, 'date', run_date=datetime.now())
+    scheduler.add_job(run_scraper, 'date', run_date=datetime.now())
+
     scheduler.start()
