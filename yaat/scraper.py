53b3a8108f6e9205c7c85710a449d15634173548
mad360 <matt@dldipaolo.com>
2025-03-02 18:11:49 -0800

    mongoengine -> beany

--- yaat/scraper.py
+++ yaat/scraper.py
@@ -1,82 +1,73 @@
-from mongoengine import Document, DateTimeField, StringField, FloatField, IntField, StringField, ReferenceField
-from pymongo import UpdateOne
 from datetime import datetime, timezone
-from typing import ClassVar
-from yaat.db import UpdateCacheDoc, InsertOnlyCacheDoc
-from marshmallow_mongoengine import ModelSchema, fields
+from beanie import Link, Document
+from pydantic import Field
+from enum import Enum
 
 #### Top Mover Docs
 
-class TopMoverQueryDoc(InsertOnlyCacheDoc):
-    durations: ClassVar[list[str]] = ['1h', '24h', '7d', '14d', '30d', '1y']
-    top_coins: ClassVar[list[str]] = ['300', '500', '1000', 'all']
+class TopMoverQueryDoc(Document):
+    class Duration(str, Enum):
+        h1 = "1h"
+        d24 = "24h"
+        d7 = "7d"
+        d14 = "14d"
+        d30 = "30d"
+        y1 = "1y"
 
-    duration = StringField(required=True, choices=durations)
-    top_coin = StringField(required=True, choices=top_coins)
+    class TopCoins(str, Enum):
+        coins_300 = "300"
+        coins_500 = "500"
+        coins_1000 = "1000"
+        coins_all = "all"
 
-class TopMoverQueryDocSchema(ModelSchema):
-    class Meta:
-        model = TopMoverQueryDoc
-        exclude = ("id",)
+    duration: Duration = Field(..., description="duration of top movers")
+    top_coin: TopCoins = Field(..., description="how many ordered coins to fetch")
+    
+    # class Settings:
+    #     name = "top_mover_docs"  # MongoDB collection name
+    #     indexes = [
+    #         "cid",  # Single-field index
+    #         [("usd", -1)]  # Compound index example: descending order on usd
+    #     ]
 
 class TopMoverDoc(Document):
     # https://docs.coingecko.com/reference/coins-top-gainers-losers
-    created_at = DateTimeField(default_factory=lambda: datetime.now(timezone.utc))
-    query = ReferenceField(TopMoverQueryDoc, required=True)
-    cid = StringField(required=True)
-    symbol = StringField(required=True)
-    name = StringField(required=True)
-    usd = FloatField(required=True)
-    market_cap_rank = IntField(required=True)
-    usd_24h_vol = IntField(required=True)
-    usd_1y_change = IntField(required=True)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    query: Link[TopMoverQueryDoc] = Field(...)
+    cid: str = Field(...)
+    symbol: str = Field(...)
+    name: str = Field(...)
+    usd: float = Field(...)
+    market_cap_rank: int = Field(...)
+    usd_24h_vol: int = Field(...)
+    usd_1y_change: int = Field(...)
 
-class TopMoverDocSchema(ModelSchema):
-    query = fields.Nested(TopMoverQueryDocSchema) 
-    class Meta:
-        model = TopMoverDoc
-        exclude = ("id",)
+    @classmethod
+    def model_json_schema(cls):
+        q = TopMoverQueryDoc.model_json_schema()
+        t = super().model_json_schema()
+        if '$defs' not in t: t['$defs'] = {}
+        q['properties'].pop('_id')
+        t['$defs'].update(q.pop('$defs') | {q['title']: q})
+        t['properties']['query'] = {'$ref': '#/$defs/'+q['title']}
+        import pprint
+        pprint.pprint(t)
+        return t
 
-class TopMoverJobDoc(UpdateCacheDoc):
-    meta = {'indexes': [{'fields': ['params'], 'unique': True}]}
+class TopMoverJobDoc(Document):
+    seconds: int = Field(...)
+    jid: int = Field(..., description='job id')
+    query: Link[TopMoverQueryDoc] = Field(...)
 
-    interval_seconds = IntField(required=True)
-    params = ReferenceField(TopMoverQueryDoc)
+class TopMoverJobDoc(Document):
+    seconds: int = Field(...)
+    jid: int = Field(..., description='job id')
+    query: Link[TopMoverQueryDoc] = Field(...)
 
-    @property
-    def update_op(self) -> UpdateOne:
-        query = {'params': self.params.pk}
-        update = {"$set": {"interval_seconds": self.interval_seconds}}
-        return UpdateOne(query, update, upsert=True)
 
-    def interval_job(self):
-        TopMoverDoc(
-            query = self.params,
-            cid = 'fdsafdsa',
-            symbol = 'btc',
-            name = 'bitcoin',
-            usd = 100,
-            market_cap_rank = 100,
-            usd_24h_vol = 100,
-            usd_1y_change = 100).save()
 
-#### Start
-
-def run():
-    from apscheduler.schedulers.background import BackgroundScheduler
-
-    # create queries
-    TopMoverQueryDoc.bulk_save([
-        TopMoverQueryDoc(duration=duration, top_coin=top_coin)
-            for duration in TopMoverQueryDoc.durations
-            for top_coin in TopMoverQueryDoc.top_coins
-    ])
-
-    # create jobs
-    TopMoverJobDoc.bulk_save([TopMoverJobDoc(interval_seconds=10*10, params=query) for query in TopMoverQueryDoc.objects()])
-
-    # start jobs
-    schedule = BackgroundScheduler()
-    for job in TopMoverJobDoc.objects():
-        schedule.add_job(job.interval_job, 'interval', seconds=job.interval_seconds)
-    schedule.start()
+    # @property
+    # def update_op(self) -> UpdateOne:
+    #     query = {'params': self.params.pk}
+    #     update = {"$set": {"interval_seconds": self.interval_seconds}}
+    #     return UpdateOne(query, update, upsert=True)
