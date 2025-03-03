757a681b7a378b628e4d37bf4aeb1549915e78aa
mad360 <matt@dldipaolo.com>
2025-03-02 18:10:30 -0800

    use beanie instead of mongoengine

--- setup.py
+++ setup.py
@@ -5,16 +5,13 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'pymongo',
         'requests',
         'apscheduler',
         'httpie',
         'dotenv',
-        'mongoengine',
+        'beanie',
         'fastapi',
         'uvicorn',
-        'marshmallow_mongoengine',
-        'marshmallow_jsonschema'
     ],
     extras_require={
         'test': [
