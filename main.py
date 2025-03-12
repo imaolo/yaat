ef974679a9b60ac919662ce17ffdf3702988554f
mad360 <matt@dldipaolo.com>
2025-03-11 18:37:30 -0700

    use state module, simplify crud endpoints, use docuimetadata

--- main.py
+++ main.py
@@ -1,97 +1,56 @@
-from fastapi import FastAPI, APIRouter, Body
-from fastapi.responses import JSONResponse, RedirectResponse
+from yaat.state import State
+from beanie import Document
+from fastapi import FastAPI, APIRouter
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
-from beanie import init_beanie, Document
-from yaat.scraper import TopMoverDoc, TopMoverQueryDoc
-from yaat.helpers import getenv
+from yaat.doc import DocUIMetadata, Doc
 import os
 
-class JobDoc(Document):
-    next_run_date: float
-    job_state: bytes
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_DIR = os.path.join(BASE_DIR, 'yaat/client/dist')
-
-document_models = [TopMoverQueryDoc, TopMoverDoc, JobDoc]
-
-def p(): print("here")
+STATIC_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'yaat/client/dist')
 
 @asynccontextmanager
-async def lifespan(_: FastAPI):
-    from apscheduler.jobstores.mongodb import MongoDBJobStore
-    from apscheduler.schedulers.background import BackgroundScheduler
-    from motor.motor_asyncio import AsyncIOMotorClient
-
-    host = 'mongo' if getenv('DOCKER', False) else 'localhost'
-    client = AsyncIOMotorClient(f"mongodb://{host}:27017")
-
-    await init_beanie(database=client['yaatdb'], document_models=document_models)
-
-    jobstore = MongoDBJobStore(client=client.delegate, database='yaatdb', collection=JobDoc.__name__)
-    scheduler = BackgroundScheduler(jobstores={'default':jobstore})
-    scheduler.start()
-
-    for duration in TopMoverQueryDoc.Duration:
-        for top_coin in TopMoverQueryDoc.TopCoins:
-            query = TopMoverQueryDoc(duration=duration, top_coin=top_coin)
-            docs = await TopMoverQueryDoc.find(query.model_dump(), limit=2).to_list()
-            match len(docs):
-                case 0: await query.create()
-                case 1: pass
-                case _: raise RuntimeError("error")
+async def lifespan(app: FastAPI):
+    await State.init()
 
     yield
 
-    scheduler.shutdown(wait=True)
+    State.terminate()
 
 app = FastAPI(title='Title', lifespan=lifespan)
-
 app.mount('/dist', StaticFiles(directory=STATIC_DIR), name='dist')
-
 app.add_route('/', lambda _: RedirectResponse(url="/dist/index.html"))
-
 app.add_route('/hb', lambda _: {'status': 'ok', 'message': "Heartbeat acknowledged"})
 
-def register_endpoint(router: APIRouter, doct: type[Document]):
+@app.get('/metadatas', response_model=list[DocUIMetadata])
+def get_metadatas() -> list[dict]:
+    return [model.get_metadata() for model in State.document_models if issubclass(model, Doc) if model.readable]
 
-    @router.get('/'+doct.__name__, response_model=list[doct])
-    async def _():
-        return await doct.find({}, projection_model=doct).to_list()
+def register_crud(router: APIRouter, schema: type[Doc]):
+    @router.post('/'+schema.__name__)
+    async def create(doc: dict):
+        doc.pop('_id', None)
+        await schema(**doc).create()
 
-    @router.post('/'+doct.__name__, response_model=doct)
-    async def _(data: dict = Body(...)):
-        await (doc:=doct(**data)).create()
-        return doc
+    print('/'+schema.__name__)
+    @router.get('/'+schema.__name__, response_model=list[schema])
+    async def read() -> list[Document]:
+        return await schema.find().to_list()
 
-    @router.get('/schema/'+doct.__name__, response_class=JSONResponse)
-    async def get_schema():
-        (schema:=doct.model_json_schema())['properties'].pop('_id', None)
-        return schema
-    
+    @router.put('/'+schema.__name__, response_model=list[schema])
+    async def update(doc: Document) -> Document:
+        raise RuntimeError("TODO not implemented")
+
+    @router.delete('/'+schema.__name__, response_model=list[schema])
+    async def delete(doc: Document) -> Document:
+        raise RuntimeError("TODO not implemented")
+
+
 router = APIRouter()
-for model in document_models:
-    register_endpoint(router, model)
+for schema in State.document_models:
+    register_crud(router, schema)
 app.include_router(router)
 
-
-# from fastapi import FastAPI
-# from fastapi_crudrouter import MongoCRUDRouter
-# from yaat.scraper import TopMoverQueryDoc
-
-# app = FastAPI()
-
-# # Use your Beanie model's underlying Motor collection.
-# collection = TopMoverQueryDoc.get_motor_collection()
-
-# app.include_router(
-#     MongoCRUDRouter(
-#         schema=TopMoverQueryDoc,
-#         collection=collection,
-#         prefix="topmover"  # This will create endpoints like /topmover/
-#     )
-# )
 
 # schema_instance = schema()
 # @router.put(prefix + "/{item_id}")
