1e868d6b9d079775d2b5e22a7709566cabebc457
mad360 <matt@dldipaolo.com>
2025-03-19 20:25:02 -0700

    remove abstract generic classes, return on insert, fix type hints, overload triggers and crud endpoints (crud for jobs)

--- yaat/scraper.py
+++ yaat/scraper.py
@@ -1,46 +1,19 @@
 from __future__ import annotations
-from yaat.state import APSJobDoc
+from yaat.state import State, APSJobDoc, JobDoc
 from yaat.doc import ReadOnlyDoc, Doc
-from datetime import datetime, timezone
-from beanie import Link, Document
-from typing import Optional, TypeVar, Generic
+from beanie import Link
+from beanie.operators import Set
+from beanie.odm.actions import before_event, Insert
 from pydantic import Field
 from enum import Enum
-from abc import ABC, abstractmethod
 from typing import ClassVar
-from beanie import Document
-from beanie.operators import Set
 from itertools import product
-
-# Abstract classes
-
-ResultT = TypeVar('ResultT', bound=Document)
-QueryT = TypeVar('QueryT', bound=Document)
-
-class Query(Generic[ResultT], ABC):
-    # TODO - override insert, prevent update, delete
-    # TODO - unique indexes
-    @abstractmethod
-    def __call__(self) -> list[ResultT]: pass
-
-class Result(Generic[QueryT], ABC):
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    query: Link[QueryT]
-
-    @classmethod
-    def __class_getitem__(cls, queryt) -> type:
-        # TODO still needed?
-        new_cls = super().__class_getitem__(queryt)
-        new_cls.queryt = queryt
-        return new_cls
-
-class JobDoc(Doc, ABC):
-    apsjob: Optional[Link[APSJobDoc]] = None
+from datetime import datetime, timezone
 
 #### Top Mover Query and Result Docs
 # https://docs.coingecko.com/reference/coins-top-gainers-losers
 
-class TopMoverQueryDoc(ReadOnlyDoc, Query['TopMoverResultDoc']):
+class TopMoverQueryDoc(ReadOnlyDoc):
     readable: ClassVar[bool] = False
 
     class Duration(str, Enum):
@@ -62,8 +35,9 @@ class TopMoverQueryDoc(ReadOnlyDoc, Query['TopMoverResultDoc']):
     duration: Duration = Field(..., description="duration of top movers")
     top_coin: TopCoins = Field(..., description="how many ordered coins to fetch")
 
-    async def insert(self, *args, **kwargs):
-        return await type(self).find_one(doc:=self.model_dump(exclude=['id'])).upsert(Set(doc), on_insert=self)
+    async def insert(self, *args, **kwargs) -> Doc:
+        await type(self).find_one(doc:=self.model_dump(exclude=['id'])).upsert(Set(doc), on_insert=self)
+        return await type(self).find_one(doc)
 
     @classmethod
     async def init(cls):
@@ -74,7 +48,7 @@ class TopMoverQueryDoc(ReadOnlyDoc, Query['TopMoverResultDoc']):
                 case 1: pass
                 case _: raise RuntimeError()
 
-    def __call__(self) -> TopMoverResultDoc:
+    def __call__(self) -> list[TopMoverResultDoc]:
         return [TopMoverResultDoc(
             query=self,
             cid='fdsafdsa',
@@ -86,8 +60,10 @@ class TopMoverQueryDoc(ReadOnlyDoc, Query['TopMoverResultDoc']):
             usd_1y_change=1
         )]
 
-class TopMoverResultDoc(ReadOnlyDoc, Query['TopMoverQueryDoc']):
+class TopMoverResultDoc(ReadOnlyDoc):
     # https://docs.coingecko.com/reference/coins-top-gainers-losers
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    query: Link[TopMoverQueryDoc]
     cid: str
     symbol: str
     name: str
@@ -96,20 +72,29 @@ class TopMoverResultDoc(ReadOnlyDoc, Query['TopMoverQueryDoc']):
     usd_24h_vol: int
     usd_1y_change: int
 
-    @classmethod
-    def model_json_schema(cls):
-        # TODO hack
-        q = TopMoverQueryDoc.model_json_schema()
-        t = super().model_json_schema()
-        if '$defs' not in t: t['$defs'] = {}
-        q['properties'].pop('_id')
-        t['$defs'].update(q.pop('$defs') | {q['title']: q})
-        t['properties']['query'] = {'$ref': '#/$defs/'+q['title']}
-        return t
-
 class TopMoverJobDoc(JobDoc):
     seconds: int
     query: Link[TopMoverQueryDoc]
 
-    # TODO add after hooks for interacting with actual scheduler
-    # TODO - crud jobs
+    @before_event(Insert)
+    async def before_create_trg(self):
+        await super().before_create_trg()
+        job = State.scheduler.add_job(self.job, 'interval', seconds=self.seconds)
+        self.apsjob = await APSJobDoc.get(job.id)
+
+    @classmethod
+    async def crud_d(cls, id: str) -> TopMoverJobDoc:
+        doc: TopMoverJobDoc = await super().crud_d(id)
+        await doc.fetch_all_links()
+        State.scheduler.remove_job(doc.apsjob.id)
+        return doc
+
+    async def job(self):
+        await self.fetch_all_links()
+        await TopMoverResultDoc.insert_many(self.query())
+
+    @classmethod
+    async def crud_c(cls, doc: dict):
+        query = await TopMoverQueryDoc(**doc['query']).insert()
+        return await cls(seconds=doc['seconds'], query=query.to_ref()).insert()
+        
