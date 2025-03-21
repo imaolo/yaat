5314909bdb2208f23afbc301408743d8f5665b41
mad360 <matt@dldipaolo.com>
2025-03-20 18:51:40 -0700

    use prime react dark mode

--- yaat/client/index.js
+++ yaat/client/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from './App.js'
+import 'primereact/./resources/themes/viva-dark/theme.css'
 
 const container = document.getElementById('index')
 const root = ReactDOM.createRoot(container)
