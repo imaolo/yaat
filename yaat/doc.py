4723bcf1336202c0fc9201e94ffb945a0eb3843c
mad360 <matt@dldipaolo.com>
2025-04-02 20:09:37 -0700

    lots and lots of simplication

--- yaat/doc.py
+++ yaat/doc.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
-from beanie import Document, Link, before_event
-from beanie.odm.actions import Insert, Update, Replace, Delete 
-from pydantic import BaseModel, create_model
-from pydantic.fields import FieldInfo
-from typing import ClassVar, Union, get_origin, get_args
-from fastapi import APIRouter, Depends, Query
+from beanie import Document, before_event, Insert, Update, Replace, Delete 
+from pydantic import BaseModel
+from fastapi import APIRouter, Depends, Body
 from fastapi_paginate import Page, Params, create_page
+from typing import ClassVar
+from dataclasses import dataclass
 from abc import ABC
 
-
-class MyParams(Params):
-    size: int = Query(50, ge=1, le=10000, description="Page size")
-
+@dataclass  
+class DocArgs:
+    schema_createable: bool = True,
+    schema_readable: bool = True,
+    schema_updateable: bool = True,
+    schema_deleteable: bool = True,
+    db_createable: bool = True,
+    db_updateable: bool = True,
+    db_deleteable: bool = True
 
 class DocUIMetadata(BaseModel):
     create: dict | None = None
@@ -26,111 +30,82 @@ class UIMetadataField:
     def __get__(self, instance: Doc, owner: type[Doc]) -> DocUIMetadata:
         if self.metadata is None:
             self.metadata = DocUIMetadata(
-                create=owner.schema_create(),
-                read=owner.schema_read(),
-                update=owner.schema_update(),
-                delete=owner.schema_delete(),
+                create=owner.schema_create() if owner.doc_args.schema_createable else None,
+                read=owner.schema_read() if owner.doc_args.schema_readable else None,
+                update=owner.schema_update() if owner.doc_args.schema_updateable else None,
+                delete=owner.schema_delete() if owner.doc_args.schema_deleteable else None,
             )
         return self.metadata
 
-class LinksEmbeddedDocTypeField:
-    def __init__(self):
-        self.doctype = None
-
-    @staticmethod
-    def get_doc_from_info(field_info: FieldInfo) -> type[Doc]:
-        org = get_origin(field_info.annotation)
-        args = get_args(field_info.annotation)
-
-        if org not in (Link, Union): return field_info.annotation
-
-        if org is Link: return args[0]
-
-        assert org is Union, org
-        if len(args) != 2: return field_info.annotation
-        if args[1] is not type(None): return field_info.annotation
-        if get_origin(args[0]) is not Link: return field_info.annotation
-        return get_args(args[0])[0]
-
-    def __get__(self, instance: Doc, owner: type[Doc]) -> type[Doc]:
-        if self.doctype is None:
-            fields = {name: (self.get_doc_from_info(field), ...) for name, field in owner.model_fields.items()}
-            self.doctype = create_model(f"{owner.__name__}__Flattened", **fields)
-        return self.doctype
-
 class Doc(Document, ABC):
-    createable: ClassVar[bool] = True
-    readable: ClassVar[bool] = True
-    updateable: ClassVar[bool] = True
-    deleteable: ClassVar[bool] = True
     ui_metadata: ClassVar[DocUIMetadata] = None # NOTE place holder
-    links_embedded: ClassVar[type[BaseModel]] = None # NOTE place holder
-    # TODO field specific?
+    # TODO field specific crud'able? right now controlled by schema, not at db level
 
-    def __init_subclass__(cls, **kwargs):
-        super().__init_subclass__(**kwargs)
-        cls.ui_metadata = UIMetadataField() # NOTE placeholder filled
-        cls.links_embedded = LinksEmbeddedDocTypeField() # NOTE placeholder filled
+    def __init_subclass__(cls, *args, doc_args: DocArgs, **kwargs):
+        super().__init_subclass__(*args, **kwargs)
+        cls.doc_args = doc_args
+        cls.ui_metadata = UIMetadataField() # NOTE fill in placeholder
+
+    # triggers TODO it would be nice to have these triggers overloadable like they are for crud endpoints
+    # current they requre the decorator in the child class
 
     @before_event(Insert)
     async def before_create_trg(self):
-        if not self.createable: raise RuntimeError()
+        if not self.doc_args.db_createable: raise RuntimeError()
 
     @before_event(Update, Replace)
     async def before_update_trg(self):
-        if not self.updateable: raise RuntimeError()
+        if not self.doc_args.db_updateable: raise RuntimeError()
 
     @before_event(Delete)
     async def before_delete_trg(self):
-        if not self.deleteable: raise RuntimeError(type(self))
+        if not self.doc_args.db_deleteable: raise RuntimeError(type(self))
+
+    # json schema generation
 
     @classmethod
-    def remove_from_schema(cls, schema: dict | None, *fields: list[str] | None) -> dict | None:
-        if schema is None: return None
+    def model_json_schema(cls, *args, exclude: set = {}, **kwargs) -> dict:
+        return cls.remove_from_schema(super().model_json_schema(*args, **kwargs), exclude)
 
+    @classmethod
+    def remove_from_schema(cls, schema: dict, fields: set = {}) -> dict:
         for name, _def in schema.get('$defs', {}).items():
             if 'type' not in _def or _def['type'] != 'object':
                 continue
-            schema['$defs'][name] = cls.remove_from_schema(_def, *fields)
-
+            schema['$defs'][name] = cls.remove_from_schema(_def, fields)
         for field in fields:
             schema['properties'].pop(field, None)
             if 'required' in schema and field in schema['required']:
                 schema['required'].remove(field)
         return schema
 
-    @classmethod
-    def get_links_embedded_schema(cls) -> dict:
-        return cls.remove_from_schema(cls.model_json_schema(), '_id', 'apsjob', 'job_state')
+    # json crud schemas
 
     @classmethod    
     def schema_create(cls) -> dict | None:
-        return cls.get_links_embedded_schema() if cls.createable else None
+        return cls.model_json_schema()
 
     @classmethod
     def schema_read(cls) -> dict | None:
-        return cls.get_links_embedded_schema() if cls.readable else None
+        return cls.model_json_schema()
 
     @classmethod
     def schema_update(cls) -> dict | None:
-        return cls.get_links_embedded_schema() if cls.updateable else None
+        return cls.model_json_schema()
 
     @classmethod
     def schema_delete(cls) -> dict | None:
-        return cls.get_links_embedded_schema() if cls.deleteable else None
+        return cls.model_json_schema()
 
-    @classmethod
-    async def init(cls):
-        pass
+    # crud endpoints handlers
 
     @classmethod
     async def crud_c(cls, doc: dict) -> None:
         await cls(**doc).create()
 
     @classmethod
-    async def crud_r(cls, params: MyParams = Depends()) -> Page[Doc]:
+    async def crud_r(cls, params: Params = Depends()) -> Page[Doc]:
         docs = await cls.find().skip((params.page-1) * params.size).limit(params.size).to_list()
-        [await doc.fetch_all_links() for doc in docs]
         return create_page(
             items=docs,
             total=await cls.find().count(),
@@ -142,10 +117,10 @@ class Doc(Document, ABC):
         raise RuntimeError("TODO not implemented")
 
     @classmethod
-    async def crud_d(cls, id: str) -> Doc:
-        doc = await cls.get(id)
-        await doc.delete()
-        return doc
+    async def crud_d(cls, ids: list[str] = Body(...)):
+        await cls.find_many({"_id": {"$in": ids}}).delete()
+
+    # configure endpoints
 
     @classmethod
     def add_crud(cls, router: APIRouter):
@@ -174,7 +149,4 @@ class Doc(Document, ABC):
             methods=["DELETE"]
         )
 
-class ReadOnlyDoc(Doc, ABC):
-    createable: ClassVar[bool] = False
-    updateable: ClassVar[bool] = False
-    deleteable: ClassVar[bool] = False
+read_only_doc_args = doc_args=DocArgs(schema_createable=False, schema_deleteable=False, schema_updateable=False)
