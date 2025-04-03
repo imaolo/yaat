41ce520c6bc8511fa7e34e884141c84d5f868b25
mad360 <matt@dldipaolo.com>
2025-04-02 20:09:50 -0700

    simplify, add new features

--- yaat/client/DocTabs.jsx
+++ yaat/client/DocTabs.jsx
@@ -2,88 +2,75 @@ import { useState, useEffect, useRef } from 'react';
 import { TabView, TabPanel } from 'primereact/tabview';
 import { DataTable } from 'primereact/datatable';
 import { Column }    from 'primereact/column';
+import { Button } from 'primereact/button';
 import $RefParser from "json-schema-ref-parser";
 import axios from 'axios'
 import Form from "@rjsf/core";
 import validator from "@rjsf/validator-ajv8";
 
-const filterOptions = {
-  string: [
-    { label: 'Contains', value: 'contains' },
-    { label: 'Equals', value: 'equals' },
-    { label: 'Not Equals', value: 'notEquals' }
-  ],
-  number: [
-    { label: '=', value: 'equals' },
-    { label: '≠', value: 'notEquals' },
-    { label: '<', value: 'lt' },
-    { label: '<=', value: 'lte' },
-    { label: '>', value: 'gt' },
-    { label: '>=', value: 'gte' }
-  ],
-  boolean: [
-    { label: 'True', value: true },
-    { label: 'False', value: false }
-  ]
-};
-
 function DocTab({ metadata }){
     const [rows, setRows] = useState([]);
+    const [selectedRows, setSelectedRows] = useState([]);
     const [first, setFirst] = useState(0);
     const [size, setSize] = useState(25);
     const [total, setTotal] = useState(0);
     const [readSchema, setReadSchema] = useState({});
+    const [loading, setLoading] = useState(true)
 
-    // manage call stage
+    // manage api call state
     const controllerRef = useRef(null);
-    const abortCall = () => {
+    const abortFetch = () => {
         if (controllerRef.current)
             controllerRef.current.abort()
     }
 
-    // form submission
-    const handleSubmit = ({ formData }) => 
-        axios
-            .post(`/${metadata.read.title}`, formData)
-            .then(() => setFirst(first))
-
-    // mount effect
-    useEffect(() => {
-        $RefParser
-            .dereference(metadata.read)
-            .then(schema => setReadSchema(schema))
-        return abortCall
-    }, [])
-
-
-
-    useEffect(() => {
+    // fetch Data helper
+    const fetchData = () => {
         const page = Math.floor(first / size) + 1;
         const params = new URLSearchParams({ page, size })
         const controller = new AbortController()
         const signal = controller.signal;
 
-        abortCall()
+        setLoading(true)
+
+        abortFetch()
         controllerRef.current = controller
 
         axios
             .get(`/${metadata.read.title}`, { signal, params })
             .then(res => {
-                console.log(res.data.items)
                 setRows(res.data.items)
                 setTotal(res.data.total)
+                setLoading(false)
             })
             .catch(err => {
-                if (!axios.isCancel(err)){
-                    console.log(err)
+                if (!axios.isCancel(err))
                     throw err
-                }
             })
+    }
+
+    // form submission
+    const handleSubmit = ({ formData }) => {
+        axios
+            .post(`/${metadata.read.title}`, formData)
+            .then(() => fetchData())
+    }
+
+    // mount hook
+    useEffect(() => {
+        $RefParser
+            .dereference(metadata.read)
+            .then(schema => setReadSchema(schema))
+        return abortFetch
+    }, [])
+
+    // paging hook
+    useEffect(() => {
+        fetchData()
     }, [first, size, readSchema]);
 
-    
+    // get datatable columns
     const getColsFromSchema = (schema, prefix = "", result = [])  => {
-        console.log(schema)
         if (!schema || !schema.properties) return result;
     
         for (const [propName, propSchema] of Object.entries(schema.properties)) {
@@ -100,65 +87,66 @@ function DocTab({ metadata }){
         return result
     }
 
+    const handleDelete = ()  => {
+        axios
+            .delete(`/${metadata.read.title}`, {data: selectedRows.map(row => row._id)})
+            .then(() => fetchData())
+    }
+
+    // TODO - update
     return (
         <div>
-          <DataTable
-            value={rows}
-            // visual
-            scrollable
-            scrollHeight="70vh"
-            resizableColumns
-            columnResizeMode='expand'
-            reorderableColumns
-            // lazy loading
-            lazy
-            // paging
-            paginator
-            first={first}
-            totalRecords={total}
-            rows={size}
-            onPage={(e) => {
-                setFirst(e.first)
-                setSize(e.rows)
-            }}
-            rowsPerPageOptions={[25, 50, 100]}
-            paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-            currentPageReportTemplate="{first} to {last} of {totalRecords}"
-            // TODO - sorting
-          >
-            {getColsFromSchema(readSchema)}
-          </DataTable>
-          <div>
-                {metadata.create != null && 
-                    <Form
-                        schema={metadata.create}
-                        validator={validator}
-                        onSubmit={handleSubmit}
-                    />
+            {metadata.delete && <Button label="Delete" onClick={handleDelete} disabled={!selectedRows.length} />}
+            <DataTable
+                // data
+                value={rows}
+                // visual
+                scrollable
+                scrollHeight="30vh"
+                resizableColumns
+                columnResizeMode='expand'
+                reorderableColumns
+                loading={loading}
+                // lazy loading
+                lazy
+                // paging
+                paginator
+                first={first}
+                totalRecords={total}
+                rows={size}
+                onPage={(e) => {
+                    setFirst(e.first)
+                    setSize(e.rows)
+                }}
+                rowsPerPageOptions={[25, 50, 100]}
+                paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+                currentPageReportTemplate="{first} to {last} of {totalRecords}"
+                // TODO - sorting
+                // selection
+                selectionMode='checkbox'
+                selection={selectedRows}
+                onSelectionChange={(e) => setSelectedRows(e.value)}
+            >
+                {metadata.delete || metadata.update ?
+                    [<Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>].concat(getColsFromSchema(readSchema)) :
+                    getColsFromSchema(readSchema)
                 }
+            </DataTable>
+        <div>
+            {metadata.create != null && 
+                <Form
+                    schema={metadata.create}
+                    validator={validator}
+                    onSubmit={handleSubmit}
+                />
+            }
             </div>
         </div>
-      );
+    );
 }
-
-{/* <DataTable
-value={rows}
-paginator
-lazy
-removableSort
-first={lazyParams.first}
-rows={lazyParams.rows}
-totalRecords={totalRows}
-rowsPerPageOptions={[25, 50, 100]}
-filterDisplay="menu"
-scrollable            // <-- enable scrolling
-scrollHeight="60vh"    
-onPage={(e) => setLazyParams({ first: e.first, rows: e.rows })}
-> */}
 
 export default function DocTabs({ metadatas }) {
     const [activeIndex, setActiveIndex] = useState(0)
-
     return (
         <TabView activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)}>
             {metadatas.map((metadata, idx) => (
