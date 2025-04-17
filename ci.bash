0253e65d82be136ffa958ce9742eb03554d4b25c
mad360 <matt@dldipaolo.com>
2025-04-17 08:05:37 -0700

    potential new ci script

---
+++ ci.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# required env vars
+: "${SSH_USERNAME:?SSH_USERNAME is required but is not set or is empty}"
+: "${SSH_HOST:?SSH_HOST is required but is not set or is empty}"
+: "${SSH_KEY:?SSH_KEY is required but is not set or is empty}"
+: "${GH_PAT:?GH_PAT is required but is not set or is empty}"
+: "${DEPLOY:?DEPLOY is required but is not set or is empty}"
+
+echo "running tests..."
+runcmd('python -m pytest tests')
+
+eccho "configuring ssh..."
+pem_file="$HOME/id_rsa.pem"
+echo "$SSH_KEY" > "$pem_file"
+chmod 600 "$pem_file"
+
+echo "Deploying remotely..."
+ssh -o StrictHostKeyChecking=no -i "$pem_file" "$SSH_USERNAME@$SSH_HOST" bash <<EOF
+set -e
+cd yaat
+git pull https://imaolo:${GH_PAT}@github.com/imaolo/yaat
+docker-compose up --build --force-recreate --remove-orphans -d --wait
+EOF
+fi
