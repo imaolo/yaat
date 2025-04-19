925df21412c71576c5d6092aca3819540bcc6bb9
mad360 <matt@dldipaolo.com>
2025-04-19 15:03:40 -0700

    scraper.py -> coingecko.py

--- yaat/__init__.py
+++ yaat/__init__.py
@@ -1,6 +1,6 @@
 from yaat.state import State
 from yaat.job import APSJobDoc
-from yaat.scraper import TopMoverJobDoc, TopMoverResultDoc
+from yaat.coingecko import TopMoverJobDoc, TopMoverResultDoc
 
 State.document_models = [
     APSJobDoc,
