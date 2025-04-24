0b5b9397fbfcbfdcda53b1ffeab391051046ae83
mad360 <matt@dldipaolo.com>
2025-04-24 14:21:12 -0700

    fixed

--- tmp.py
+++ tmp.py
@@ -1,134 +1,127 @@
-from __future__ import annotations
-from yaat.doc import Doc, DocArgs
-from yaat.helpers import fetchjson
-from pydantic import BaseModel, Field, field_serializer
-from enum import Enum
-from datetime import datetime, timezone
-from yaat.job import IntervalJobDoc
-from datetime import datetime, timedelta as td
+from yaat.state import State
+from yaat.coingecko import CoinGecko, TopMoverResultDoc
+from datetime import datetime as dt, timedelta as td
+from typing import Any
+from pprint import pprint
+import asyncio, numpy as np, pandas as pd
 
-class CoinGecko:
-    url: str = 'https://pro-api.coingecko.com/api/v3/'
-    key : str = 'CG-ivjvmcDsabTGQg25HpTUa7H5'
+window_size=4
+cushion_len = td(minutes=5)
+window_len = td(minutes=15)
 
-    @classmethod
-    async def fetch(cls, ext, **kwargs):
-        return await fetchjson(cls.url + ext,
-                               headers={"accept": "application/json", "x-cg-pro-api-key": cls.key},
-                               **kwargs)
+async def get_percent_change_hour(mover: TopMoverResultDoc) -> float | None:
+    cg_data = await CoinGecko.historical_chart_range(mover.cid, vs_currency='usd', **{'from':(now:=mover.created_at).timestamp()},
+                                                     to=(now + td(hours=2)).timestamp(), precision='10')
 
-    @classmethod
-    async def top_gainers_losers(cls, **kwargs) -> dict:
-        return await cls.fetch('coins/top_gainers_losers', **kwargs)
+    try:
+        start_price = cg_data['prices'][0][1]
+        end_price = cg_data['prices'][-1][1]
+        return ((end_price - start_price) / start_price)*100
+    except:
+        print('exception!')
+        print(mover)
+        pprint(cg_data)
+        return None
 
-    @classmethod
-    async def coin_data(cls, cid:str, **kwargs) -> dict:
-        return await cls.fetch(f"coins/{cid}", **kwargs)
+async def get_training_point(doc: TopMoverResultDoc) -> dict | None:
+    times = [doc.created_at]
+    or_conds = []
+    for _ in range(window_size-1):
+        times.append(times[-1]-window_len)
+        or_conds.append({'$and': [
+            {'created_at': {'$gte': times[-1] - cushion_len}},
+            {'created_at': {'$lte': times[-1] + cushion_len}}
+        ]})
 
-    @classmethod
-    async def historical_chart_range(cls, cid, **kwargs) -> dict:
-        return await cls.fetch(f"coins/{cid}/market_chart/range/", **kwargs)
-
-class TopMoverQueryDoc(BaseModel):
-    class Duration(str, Enum):
-        h1 = "1h"
-        h24 = "24h"
-        d7 = "7d"
-        d14 = "14d"
-        d30 = "30d"
-        y1 = "1y"
-
-    class TopCoins(str, Enum):
-        coins_300 = "300"
-        coins_500 = "500"
-        coins_1000 = "1000"
-        coins_all = "all"
-
-    vs_currency: str = 'usd'
-    duration: Duration = Field(..., description="duration of top movers")
-    top_coin: TopCoins = Field(..., description="how many ordered coins to fetch")
-
-    @field_serializer('duration')
-    def duration_field_serializer(self, duration: Duration) -> str: return duration.value
-    
-    @field_serializer('top_coin')
-    def top_coin_field_serializer(self, top_coin: TopCoins) -> str: return top_coin.value
-
-    @property
-    def timedelta(self) -> td:
-        match self.duration:
-            case self.Duration.h1: return td(hours=1)
-            case self.Duration.h24: return td(hours=24)
-            case self.Duration.d7: return td(days=7)
-            case self.Duration.d14: return td(days=14)
-            case self.Duration.d30: return td(days=30)
-            case self.Duration.y1: return td(weeks=52)
-            case _: raise RuntimeError(self.duration)
-
-class TopMoverResultDoc(Doc, doc_args=DocArgs(schema_updateable=False, db_updateable=False)):
-    # https://docs.coingecko.com/reference/coins-top-gainers-losers
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    query: TopMoverQueryDoc
-    cid: str
-    symbol: str
-    name: str
-    usd: float
-    market_cap_rank: int
-    usd_24h_vol: float
-    percent_change: float
-
-    @field_serializer('created_at')
-    def serialize_created_at(self, dt: datetime, _info) -> str:
-        if dt.tzinfo is None:
-            dt = dt.replace(tzinfo=timezone.utc)
+    prev = await TopMoverResultDoc.aggregate([{'$match': {'$or': or_conds, 'cid': doc.cid}}]).to_list()
+    prev = list(map(lambda d: TopMoverResultDoc(**d), prev))
+    training_set = {times[0]: doc}
+    curr_prev_idx = 0
+    for time in times[1:]:
+        if (curr_prev_idx < len(prev)) and ((time - cushion_len) < prev[curr_prev_idx].created_at < (time + cushion_len)):
+            training_set[time] = prev[curr_prev_idx]
+            curr_prev_idx += 1
         else:
-            dt = dt.astimezone(timezone.utc)
-        return dt.isoformat().replace("+00:00", "Z")
+            training_set[time] = None
 
-    class Settings:
-        indexes = [
-            [("created_at", -1), ('query.duration', 1), ('query.top_coin', 1)]
-        ]
+    X: list[TopMoverResultDoc | None] = training_set.values()
+    Y: float = await get_percent_change_hour(doc)
 
-class TopMoverJobDoc(IntervalJobDoc, doc_args=DocArgs()):
-    query: TopMoverQueryDoc
+    if Y is not None:
+        def get_training_fields(i, x:TopMoverResultDoc | None) -> dict[str, Any]:
+            if x:
+                return {k+'-'+str(i): v for k, v in x.model_dump().items()}
+            else:
+                return {k+'-'+str(i): np.nan for k in TopMoverResultDoc.model_fields.keys()}
 
-    @classmethod
-    async def crud_c(cls, doc: dict) -> None:
-        await cls(query=TopMoverQueryDoc(**doc.pop('query')), **doc).create()
+        return {
+            'percent_change': Y,
+            **{k:v for i, x in enumerate(X) for k, v in get_training_fields(i, x).items()}
+        }
+    
+def create_df(records: list[dict]):
+    df = pd.DataFrame(records)
+    df = df.drop(columns=df.select_dtypes(exclude=['number']).columns)
+    df = df.dropna(axis=1, how='all')  # drop all-NaN columns
+    df = df.drop(columns=[col for col in df.columns if 'usd-' in col])
+    return df
 
-    async def func(self):
-        # https://docs.coingecko.com/reference/coins-top-gainers-losers
-        cg_data: list[dict] = (await CoinGecko.top_gainers_losers(**self.query.model_dump()))
-        created_at = datetime.now(timezone.utc)
-        top_movers: list = cg_data['top_gainers']
-        top_movers.extend(cg_data['top_losers'])
-        result_docs: list = []
-        date = datetime.now() - self.query.timedelta
-        for tm in top_movers:
-            tm['cid'] = tm.pop('id')
-            tm.pop('image')
-            async def get_percent_change(cid: str) -> float:
-                cg_data = await CoinGecko.historical_chart_range(cid, vs_currency='usd', **{'from':date.timestamp()}, to=(date + td(minutes=5)).timestamp(), precision='10')
-                midpoint = date
-                increments = (datetime.now() - midpoint)/8
-                while not cg_data['prices'] and midpoint < datetime.now() - td(minutes=4):
-                    print(f"searching for valid date ({midpoint}) for {tm['cid']}")
-                    midpoint = midpoint + increments
-                    cg_data = await CoinGecko.historical_chart_range(cid, vs_currency='usd', **{'from':midpoint.timestamp()}, to=(midpoint + td(minutes=5)).timestamp(), precision='10')
-                try: _, old_price = cg_data['prices'][0]
-                except:
-                    from pprint import pprint
-                    pprint(cg_data)
-                    pprint(tm)
-                    raise
-                cg_data = await CoinGecko.coin_data(cid, tickers='false', community_data='false', developer_data='false', market_data='true')
-                current_price = cg_data['market_data']['current_price']['usd']
-                return ((current_price-old_price)/old_price)*100
-            tm['percent_change'] = await get_percent_change(tm['cid'])
-            result_docs.append(TopMoverResultDoc(query=self.query, created_at=created_at, **tm))
-        await TopMoverResultDoc.insert_many(result_docs)
+async def get_set(agg: Any) -> pd.DataFrame:
+    ret = []
+    i = 0
+    async for doc in agg:
+        print(i); i+=1
+        point = await get_training_point(doc)
+        if point:
+            ret.append(point)
+    return pd.DataFrame(ret)
 
+def get_x_y_from_df(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+    return df.drop(columns='percent_change'), df['percent_change']
+    
+async def get_x_y_from_agg(agg: Any) -> tuple[pd.DataFrame, pd.Series]:
+    return get_x_y_from_df(await get_set(agg))
+
+async def train() -> tuple[Any, Any, Any]:
+    X, y = await get_x_y_from_agg(TopMoverResultDoc.find().sort(('created_at',1),).limit(1000))
+
+    from sklearn.ensemble import HistGradientBoostingRegressor
+    from sklearn.preprocessing import RobustScaler
+
+    x_scaler = RobustScaler()
+    X_scaled = x_scaler.fit_transform(X)
+    y_scaler = RobustScaler()
+    y_scaled = y_scaler.fit_transform(y.values.reshape(-1, 1)).ravel()
+
+    model = HistGradientBoostingRegressor()
+    model.fit(X_scaled, y_scaled)
+
+    return model, x_scaler, y_scaler
+
+async def pred(model: Any, x_scaler: Any, y_scaler: Any):
+    X, y = await get_x_y_from_agg(TopMoverResultDoc.find().sort(('created_at', 1),).skip(1000).limit(250))
+
+    X_scaled = x_scaler.transform(X)
+    y_scaled = y_scaler.transform(y.values.reshape(-1, 1)).ravel()
+
+    preds = model.predict(X_scaled)
+
+    from sklearn.metrics import mean_squared_error, r2_score
+
+    mse = mean_squared_error(y_scaled, preds)
+    r2 = r2_score(y_scaled, preds)
+
+    print(f"MSE: {mse:.4f}, R²: {r2:.4f}")
+
+async def main():
+
+    await State.init_beanie()
+
+    model, x_scaler, y_scaler = await train()
+
+    await pred(model, x_scaler, y_scaler)
+
+asyncio.run(main())
 
 
     # window_size = 4
