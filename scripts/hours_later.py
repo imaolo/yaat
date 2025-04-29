f821119fa422b0c985a90e1a408efad84027ddb5
mad360 <matt@dldipaolo.com>
2025-04-28 22:59:40 -0700

    creation

---
+++ scripts/hours_later.py
@@ -0,0 +1,31 @@
+from yaat.state import State
+from yaat.coingecko import TopMoverResultDoc, CoinGecko
+from datetime import timedelta, datetime
+import asyncio
+
+async def main():
+    await State.init_beanie()
+
+    TopMoverResultDoc.doc_args.db_updateable = True
+
+    hour = timedelta(hours=1.0)
+    hour_2 = timedelta(hours=2.0)
+    async for doc in TopMoverResultDoc.find():
+
+        diff = datetime.now() - doc.created_at
+        if diff > hour:
+            to = doc.created_at + timedelta(hours=1.0)
+            cg_data = await CoinGecko.historical_chart_range(doc.cid, vs_currency='usd', **{'from':(to-hour_2).timestamp()}, to=to.timestamp(), precision='10')
+            try: _, hour_later_price = cg_data['prices'][-1]
+            except:
+                print("except!")
+                print(doc)
+            doc.hour_later_change = ((hour_later_price - doc.usd)/doc.usd)*100
+            await doc.save()
+            print(doc.symbol, " : ", doc.hour_later_change)
+        else:
+            print('skipped: ', doc.symbol)
+
+    TopMoverResultDoc.doc_args.db_updateable = False
+
+asyncio.run(main())
